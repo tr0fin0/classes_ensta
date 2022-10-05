345c631533bf2eadc338184f7417cbb344f4fb6f
tr0fin0 <guitrofino@gmail.com>
2022-10-05 19:46:31 +0200

    💡 change commits position

--- 2A/IN204/example/pointer.cpp
+++ 2A/IN204/example/pointer.cpp
@@ -9,24 +9,12 @@ int main() {
     pointVar = &var;
 
     cout << "variable" << endl;
-    cout << "value:    var = " <<  var << endl;
-    cout << "address: &var = " << &var << endl << endl;
+    cout << "value:    var = " <<  var << endl;             //  var = 5
+    cout << "address: &var = " << &var << endl << endl;     // &var = 0x61fe14
 
 
     cout << "pointer" << endl;
-    cout << "address, pointVar = " <<  pointVar << endl;
-    cout << "value,  *pointVar = " << *pointVar << endl;
+    cout << "address, pointVar = " <<  pointVar << endl;    //  pointVar = 0x61fe14
+    cout << "value,  *pointVar = " << *pointVar << endl;    // *pointVar = 5
 
     return 0;
-}
-
-// OUTPUT________________________
-// pointer arithmetic
-
-// variable
-// value:    var = 5
-// address: &var = 0x61fe14
-
-// pointer
-// address, pointVar = 0x61fe14
-// value,  *pointVar = 5
