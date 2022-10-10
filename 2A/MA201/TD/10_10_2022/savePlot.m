b0e9320e3dd9bcb1e394b6524b6f8dbbf7fb9a39
tr0fin0 <guitrofino@gmail.com>
2022-10-10 20:47:48 +0200

    ✨ specialize savePlot function

--- 2A/MA201/TD/10_10_2022/savePlot.m
+++ 2A/MA201/TD/10_10_2022/savePlot.m
@@ -1,27 +1,7 @@
-function savePlot(titleText, legendText, saveName, xLabel, yLabel)
+function savePlot(saveName)
+
     fileType = 'png';
-    legendLocation = "southeast";
     pPath = 'C:\Users\Admin-PC\Documents\git_repository\classes_ensta\2A\MA201\TD\10_10_2022\images';
-    pSize = [0 0 18 18];
 
-
-    if not(isempty(titleText))
-        title(titleText, 'Interpreter','tex')
-    end
-
-    if not(isempty(legendText))
-        legend(legendText, "location", legendLocation, 'Interpreter', 'tex')
-    end
-
-    if not(isempty(xLabel))
-        xlabel(xLabel);
-    end
-
-    if not(isempty(yLabel))
-        ylabel(yLabel);
-    end
-
-    grid on;
-    set(gcf, 'PaperPosition', pSize);
     saveas(gca, fullfile(pPath, saveName), fileType);
 end
