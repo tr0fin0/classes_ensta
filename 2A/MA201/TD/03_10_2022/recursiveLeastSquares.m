7a64ca8c87c32c304bed91623e77cfa614b2a36d
tr0fin0 <guitrofino@gmail.com>
2022-10-10 09:15:05 +0200

    ✨ add equations

--- 2A/MA201/TD/03_10_2022/recursiveLeastSquares.m
+++ 2A/MA201/TD/03_10_2022/recursiveLeastSquares.m
@@ -15,34 +15,28 @@ function theta = recursiveLeastSquares(input)
 
 
     % initial conditions
-    theta_0 = zeros(3,1);
-    P_0 = 1000 * eye(3);
+    theta = zeros(3,1);
+    P = eye(3);
+    R = sigmab * eye(N);
 
-    % initialization
-    theta_n = theta_0;
-    P_n = P_0;
+    % setup
+    H  = [
+        cos(om1 * Te * (1:N));
+        cos(om2 * Te * (1:N));
+        cos(om3 * Te * (1:N));
+    ]';
+    theory = (H'*H) \ (H'*Z);
 
-    % algorithm
-    for n = 0:N-1
-        % 
-        m_np1 = 0;
-        R_np1 = sigmab;
-        H_np1 = [cos(om1*(n+1)*Te), cos(om2*(n+1)*Te), cos(om3*(n+1)*Te)];
-
-        % recursive
-        size(H_np1)
-        size(P_n)
-        size(R_np1)
-        size(S_np1)
-        S_np1       = H_np1 * P_n * H_np1' + R_np1;
-        K_np1       = P_n * H_np1 / S_np1;
-        theta_np1   = theta_n + K_np1 * (Z(n+1) - m_np1 - H_np1 * theta_n);
-        P_np1       = P_n - K_np1 * H_np1 * P_n;
-
-        % 
-        theta_n = theta_np1;
-        P_n = P_np1;
+    % recursion
+    for i = 1:N
+        [P, theta] = rec(P, theta, H, Z, R);
     end
+    theory
 
-    theta = theta_n;
+    function [P, theta] = rec(P, theta, H, Z, R)
+        S = H * P * H' + R;
+        K = (P * H') / S;
+        theta = theta + K * (Z - H*theta);
+        P = P - K*H*P;
+    end
 end
