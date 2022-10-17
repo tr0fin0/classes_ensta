42c59a42b03094f1cae94b0c0816f78875843939
tr0fin0 <guitrofino@gmail.com>
2022-10-17 09:56:49 +0200

    ✨ add main code

---
+++ 2A/MA201/TD/2022_10_17/ma201_pc6.m
@@ -0,0 +1,16 @@
+%%============================================================================
+%%                    Main Code
+%%============================================================================
+clc
+clear all
+close all
+
+ID = 217276;
+feature('DefaultCharacterSet','UTF-8')
+
+%   Parameters Estimation by Noisly Sinus
+%   Import data from PC6 moodle files
+filtrePart = load('filtrePart.mat');
+
+
+% exercice1(filtrePart);
