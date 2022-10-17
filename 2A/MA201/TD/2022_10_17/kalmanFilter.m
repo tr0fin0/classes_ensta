33b036fc14bef1a1d4051837f19c07f43d1abe5a
tr0fin0 <guitrofino@gmail.com>
2022-10-17 10:42:13 +0200

    ✨ add kalmanFilter.m

---
+++ 2A/MA201/TD/2022_10_17/kalmanFilter.m
@@ -0,0 +1,33 @@
+function [x, t] = kalmanFilter(N, Z, F, H, W, V)
+    % ========================================
+    % Kalman Filter
+    % ========================================
+
+
+    % inicialization
+    x  = [];
+    t  = [];
+    xk = [0; 0; 0; 0];
+    Pk = 1000 * eye(4);
+    % TODO automatic sizing
+
+
+    for i = 1 : N
+        % prediction
+        xk = F * xk;
+        Pk = F * Pk * F' + W;
+
+        % correction
+        K  = Pk * H' * inv(H * Pk * H' + V);
+
+        % update
+        xk = xk + K * (Z(:,i) - H * xk);
+        Pk = (eye(size(K*H)) - K * H) * Pk;
+
+        % saving
+        x = [x, xk];
+        t = [t, trace(Pk)];
+    end
+    % x     % debuging
+    % t     % debuging
+end
