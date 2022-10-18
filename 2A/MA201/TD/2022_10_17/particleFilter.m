41276b38810173879c879423aa88a45a85dc60dc
tr0fin0 <guitrofino@gmail.com>
2022-10-18 09:47:36 +0200

    ✨ add equations

--- 2A/MA201/TD/2022_10_17/particleFilter.m
+++ 2A/MA201/TD/2022_10_17/particleFilter.m
@@ -1,4 +1,4 @@
-function [x, t] = particleFilter(N, Z, F, H, W, V)
+function [x, t] = particleFilter(Nr, N, Z, F, H, W, V)
 
     % ========================================
     % Particules Filter
@@ -11,26 +11,42 @@ function [x, t] = particleFilter(N, Z, F, H, W, V)
     x  = [];
     t  = [];
 
-    xk = [0; 0; 0; 0];
-    Pk = 1000 * eye(4);
+    x0 = [0; 0; 0; 0];
+    P0 = 1/Nr * eye(4);
+
+    Pk = P0;
+    xk = x0;
     % TODO automatic sizing
+    
+    
+    % particles generation
+    size(F)
+    size(Pk)
+    size(sqrtm(Pk))
+    size(Nr)
+    size(randn(Nr))
+    xki = F + randn(Nr) .* sqrtm(Pk)
+    
+    % error calculation
+    eki = zk - H
+    
+    % vraisemblance calculation
+    Pp = 1/(Nr^2) * P0;
+    M = H * Pp * H' + V
+    qi = (1/(sqrt(2*pi))) * exp(-(1/2)*(eki' * inv(M) * eki)) % pi ^ n where n is ??
+    
+    for k = 1 : N
+        for i = 1 : Nr
+            xki = xk + sqrt(Pk) * randn(4, 1);
+            xki = F + sqrt(W) * randn(4, 1);
 
+            ek = zk - hk;
+            qi = eki; % p*(eki)
+            wki = qi / sum(qi)
+        end
 
-    for i = 1 : N
-        % prediction
-        xk = F * xk;
-        Pk = F * Pk * F' + W;
-
-        % correction
-        K  = Pk * H' * inv(H * Pk * H' + V);
-
-        % update
-        xk = xk + K * (Z(:,i) - H * xk);
-        Pk = (eye(size(K*H)) - K * H) * Pk;
-
-        % saving
-        x = [x, xk];
-        t = [t, trace(Pk)];
+        xk = sum(wki * xki);
+        Pk = sum(wki * (xk - xki) * (xk - xki)');
     end
     % x     % debuging
     % t     % debuging
