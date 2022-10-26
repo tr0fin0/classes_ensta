cde407505de57f6bcfe5be7d5005c9b0bfc57843
tr0fin0 <guitrofino@gmail.com>
2022-10-26 10:04:42 +0200

    🚧 add decCounter body

---
+++ 2A/IN204/TD/2022_10_26/decCounter.hpp
@@ -0,0 +1,19 @@
+#ifndef DECCOUNTER_HPP
+#define DECCOUNTER_HPP
+#include "counter.hpp"
+ 
+class decCounter : public counter
+{
+
+public:
+    void dec() { i--; }
+ 
+protected:
+    decCounter(): counter() {}
+    decCounter(unsigned counter): counter(counter) {}
+    decCounter(const counter& otherCounter): counter(otherCounter.i) {}
+    ~decCounter() {}
+
+};
+
+#endif // DECCOUNTER_HPP
