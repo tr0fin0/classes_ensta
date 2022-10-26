c7e246e4e4e39661778b6b4fff9957f631b14a5f
tr0fin0 <guitrofino@gmail.com>
2022-10-26 10:12:03 +0200

    🚧 add allCounter body

---
+++ 2A/IN204/TD/2022_10_26/allCounter.hpp
@@ -0,0 +1,25 @@
+#ifndef ALLCOUNTER_HPP
+#define ALLCOUNTER_HPP
+#include "incCounter.hpp"
+#include "decCounter.hpp"
+ 
+class allCounter : public decCounter, incCounter
+{
+
+public:
+ 
+protected:
+    allCounter(): 
+        decCounter(), 
+        incCounter() {}
+    allCounter(unsigned counter): 
+        decCounter(counter),
+        incCounter(counter) {}
+    allCounter(const counter& otherCounter): 
+        decCounter(otherCounter.i), 
+        incCounter(otherCounter.i) {}
+    ~allCounter() {}
+
+};
+
+#endif // INCCOUNTER_HPP
