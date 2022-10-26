959d65da8b6ebf2f1879df6f0be86b492b50b571
tr0fin0 <guitrofino@gmail.com>
2022-10-26 09:50:35 +0200

    🚧 inicialize main file

---
+++ 2A/IN204/TD/2022_10_26/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "counter.hpp"
+#include "decCounter.hpp"
+#include "incCounter.hpp"
+#include "allCounter.hpp"
+
+int main() {
+    std::cout << "TD 07/09/2022\n";
+
+
+    return 0;
+}
