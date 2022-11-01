38c3c6b661ae8898438d97cb66547709529bbbd8
tr0fin0 <guitrofino@gmail.com>
2022-11-01 18:51:16 +0100

    💡 add code explanation

--- 2A/MA201/project/ma201_projet.m
+++ 2A/MA201/project/ma201_projet.m
@@ -6,13 +6,18 @@ clear all
 close all
 
 ID = 217276;
-feature('DefaultCharacterSet','UTF-8')
+% feature('DefaultCharacterSet','UTF-58')   % not compatible with octave
 
 %   Kalman Filter Simple
 %   Import data from project files
-mesuresK1 = load('mesurestrajKalm1.mat')
+mesuresK1 = load('mesurestrajKalm1.mat');
 mesuresK2 = load('mesurestrajKalm2.mat');
 mesuresK3 = load('mesurestrajKalm3.mat');
 
 
+%   ========================================
+%   we studied a simple tracking system of a radar and a target, details in the
+%   project report.
+
 exercice1(mesuresK1);
+exercice1(mesuresK2);
