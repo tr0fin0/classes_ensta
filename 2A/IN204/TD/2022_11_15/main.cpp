fd7ec10f47b5aeb9f2d2a2b0f7b8263af9413577
tr0fin0 <guitrofino@gmail.com>
2022-11-15 12:14:03 +0100

    🚧 add thread code

--- 2A/IN204/TD/2022_11_15/main.cpp
+++ 2A/IN204/TD/2022_11_15/main.cpp
@@ -1,7 +1,41 @@
+#include <thread>
 #include <iostream>
 
-int main() {
-    std::cout << "main definition" << std::endl;
+int var = 0;
 
+int max(int n, int m) {
+    if (n > m) {
+        return n;
+    }
+    else {
+        return m;
+    };
+}
+
+
+void enumerate(int n) {
+    std::cout << "n = " << n << ": ";
+
+    int i = 0;
+    for (i; i < max(var, n); i++) {
+        std::cout << i << " ";
+    }
+    var += i;
+
+    std::cout << "(n, v) = (" << n << ", " << var << ")" << std::endl;
+};
+
+
+int main() {
+    std::cout << "TD 15/11/2022" << std::endl;
+
+    std::thread th1(enumerate, 7);
+    std::thread th2(enumerate, 6);
+    std::thread th3(enumerate, 5);
+    th1.join();
+    th2.join();
+    th3.join();
+
+    std::cout << var;
     return 0;
 }
