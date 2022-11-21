f73a7bc2b64f3b4647a3ea10b3965e61405750ce
tr0fin0 <guitrofino@gmail.com>
2022-11-21 16:35:46 +0100

    🚧 add question 9

--- 2A/OS201/TD/2022_11_21/main.c
+++ 2A/OS201/TD/2022_11_21/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "utils.h"
+// question 9
+#include <fcntl.h>
+#include <unistd.h>
 
 
 // question 1
@@ -10,6 +13,10 @@ char stack_2[STACK_SIZE];
 char stack_3[STACK_SIZE];
 
 
+int main(void) {
+    printf("TD 21/11/2022\n");
+    // question 9
+    fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
 
     // question 1
     printf("0x%p\n",(char *) &stack_0);
@@ -26,9 +33,11 @@ char stack_3[STACK_SIZE];
     enter_coroutine(routine_1);
 
 
-    // 
-    char stack[STACK_SIZE];
+    // question 9
+    /*
+    comment getchar() bloque le fonctionnement du système, les threads seront arretes quand il reçoit en entre de clavier. donc le processus s'arret
+    */
 
 
     return 0;
-}
+};
