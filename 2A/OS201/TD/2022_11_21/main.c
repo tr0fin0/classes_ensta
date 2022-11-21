2a180caae84afb47f690f03020cfe6baf80c93eb
tr0fin0 <guitrofino@gmail.com>
2022-11-21 09:18:54 +0100

    ✨ add main file

---
+++ 2A/OS201/TD/2022_11_21/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "utils.h"
+
+
+#define STACK_SIZE 4096
+
+
+
+int main(void) {
+    printf("TD 21/11/2022");
+
+    // 
+    char stack[STACK_SIZE];
+
+
+    return 0;
+}
