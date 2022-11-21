eca0ea51111ace566516fc891170b86a02daec49
tr0fin0 <guitrofino@gmail.com>
2022-11-21 16:30:59 +0100

    ✨ add question 1 changes

--- 2A/OS201/TD/2022_11_21/main.c
+++ 2A/OS201/TD/2022_11_21/main.c
@@ -2,8 +2,20 @@
 #include "utils.h"
 
 
+// question 1
 #define STACK_SIZE 4096
+char stack_0[STACK_SIZE];
+char stack_1[STACK_SIZE];
+char stack_2[STACK_SIZE];
+char stack_3[STACK_SIZE];
 
+
+
+    // question 1
+    printf("0x%p\n",(char *) &stack_0);
+    printf("0x%p\n",(char *) &stack_1);
+    printf("0x%p\n",(char *) &stack_2);
+    printf("0x%p\n",(char *) &stack_3);
 
 
 int main(void) {
