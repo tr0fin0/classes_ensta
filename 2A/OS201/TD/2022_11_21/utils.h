29f112db8103cd68cf3faa8d159f9f4befa3b6c3
tr0fin0 <guitrofino@gmail.com>
2022-11-21 16:35:03 +0100

    🔥 remove asm code

--- 2A/OS201/TD/2022_11_21/utils.h
+++ 2A/OS201/TD/2022_11_21/utils.h
@@ -6,19 +6,6 @@ void enter_coroutine(coroutine_t cr);
 /* 
     Quitte le contexte courant et charge les registres et la pile de CR.
 */
-    .global enter_coroutine // visible to the linker
-    enter_coroutine:
-    mov %rdi, %rsp  /* RDI contains the argument to enter_coroutine. */
-                    /* And is copied to RSP. */
-    pop %r??
-    pop %r??
-    pop %r??
-    pop %r??
-    pop %r??
-    pop %r??
-    ret             /* Pop the program counter */
-};
-
 
 // question 5
 void switch_coroutine(coroutine_t *p_from, coroutine_t to);
