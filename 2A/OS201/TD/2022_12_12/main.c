19de1012d1982471ca4f68fcd2577163da4f9ee9
tr0fin0 <guitrofino@gmail.com>
2022-12-12 16:53:03 +0100

    🚧 add main code

--- 2A/OS201/TD/2022_12_12/main.c
+++ 2A/OS201/TD/2022_12_12/main.c
@@ -7,6 +7,42 @@ char out_buffer[100];
 
 int main(void) {
 
+    // variables
+    char *ptr = &in_buffer[0];
+    int a, b, num;
+
+    printf("main\n");
+
+
+    while(1) {
+        read(STDIN_FILENO, in_buffer, 1000);
+
+        switch (*ptr++)
+        {
+        case '+':
+            // printf("+\n");
+            sscanf(ptr, "%d,%d", &a, &b);
+
+            num = sprintf(out_buffer, "result: %d\n", a+b);
+
+            write(STDOUT_FILENO, out_buffer, num);
+            break;
+        
+        case '-':
+            // printf("-\n");
+            sscanf(ptr, "%d,%d", &a, &b);
+
+            num = sprintf(out_buffer, "result: %d\n", a-b);
+
+            write(STDOUT_FILENO, out_buffer, num);
+            break;
+
+        default:
+            // printf("default\n");
+            break;
+        };
+    };
+
     // int sscanf(const char *str, const char *format, ...);
     // int sprintf(char *str, const chart *format, ...);
 
