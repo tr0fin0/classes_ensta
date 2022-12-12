72170ac50b4382bd042c36a6d2cb92d1e4233b3b
tr0fin0 <guitrofino@gmail.com>
2022-12-12 11:10:30 +0100

    ✨ add main file

---
+++ 2A/OS201/TD/2022_12_12/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(void) {
+
+    // int sscanf(const char *str, const char *format, ...);
+    // int sprintf(char *str, const chart *format, ...);
+
+    return 0;
+};
