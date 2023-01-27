f02f4a6ba7ee8727474b104f9540ffa138190b35
tr0fin0 <guitrofino@gmail.com>
2023-01-27 07:33:29 +0100

    🎨 add spacing

--- 2A/OS202/TD/2023_01_24/ProdMatMat.cpp
+++ 2A/OS202/TD/2023_01_24/ProdMatMat.cpp
@@ -7,15 +7,21 @@
 #endif
 #include "ProdMatMat.hpp"
 
+
 namespace {
-void prodSubBlocks(int iRowBlkA, int iColBlkB, int iColBlkA, int szBlock,
-                   const Matrix& A, const Matrix& B, Matrix& C) {
-  for (int j = iColBlkB; j < std::min(B.nbCols, iColBlkB + szBlock); j++)
-    for (int i = iRowBlkA; i < std::min(A.nbRows, iRowBlkA + szBlock); ++i)
-      for (int k = iColBlkA; k < std::min(A.nbCols, iColBlkA + szBlock); k++)
-        C(i, j) += A(i, k) * B(k, j);
-}
-const int szBlock = 32;
+  void prodSubBlocks(int iRowBlkA, int iColBlkB, int iColBlkA, int szBlock,
+                    const Matrix& A, const Matrix& B, Matrix& C) {
+
+    #pragma omp parallel
+    int i, j, k;
+    #pragma omp for
+
+    for (i = iRowBlkA; i < std::min(A.nbRows, iRowBlkA + szBlock); ++i)
+      for (j = iColBlkB; j < std::min(B.nbCols, iColBlkB + szBlock); j++)
+        for (k = iColBlkA; k < std::min(A.nbCols, iColBlkA + szBlock); k++)
+          C(i, j) += A(i, k) * B(k, j);
+  }
+  const int szBlock = 32;
 }  // namespace
 
 Matrix operator*(const Matrix& A, const Matrix& B) {
