5e713e74b1f0e3ec85c0069bfe63eb6073ac9bd8
tr0fin0 <guitrofino@gmail.com>
2023-01-30 08:05:42 +0100

    ✨ add serialWhile fucntion

--- 2A/OS202/TD/2023_01_24/MPI.py
+++ 2A/OS202/TD/2023_01_24/MPI.py
@@ -1,5 +1,23 @@
 from mpi4py import MPI
 
+PROCESS_MAX     = 10
+INTERATION_MAX  = 25
+def serialWhile() -> None:
+    interation  = 0
+    process     = 0
+    value       = 0
+
+
+    while(interation < INTERATION_MAX):
+
+        if (process+1) == PROCESS_MAX:
+            process = 0
+            print('='*31)
+        else:
+            print(f'process {process:2.0f} send {value:4.0f} to rank {process+1:2.0f}')
+            process     += 1
+            value       += 1
+            interation  += 1
 def parallelWhile(value: int):
     comm = MPI.COMM_WORLD   # instantice the communication world
     size = comm.Get_size()  # size of the communication world
