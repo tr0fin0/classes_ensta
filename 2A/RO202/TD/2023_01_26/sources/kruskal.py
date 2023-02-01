4a414e31550d4492aa511279cf9c18a7a3f2fecd
tr0fin0 <guitrofino@gmail.com>
2023-02-01 20:47:33 +0100

    🔥 remove some comments

--- 2A/RO202/TD/2023_01_26/sources/kruskal.py
+++ 2A/RO202/TD/2023_01_26/sources/kruskal.py
@@ -3,13 +3,10 @@ import graph
 import sys
 
 def main():
-    
-    # Créer un graphe contenant les sommets a, b, c, d, e, f, g 
     g = graph.Graph(np.array(["a", "b", "c", "d", "e", "f", "g"]))
 
-    # Ajouter les arêtes
     g.addEdge("a", "b",  1.0)
-    g.addEdge("a", "c",  3.0)
+    g.addEdge("a", "c",  8.0)
     g.addEdge("b", "c",  2.0)
     g.addEdge("b", "d",  5.0)
     g.addEdge("b", "e",  7.0)
@@ -20,21 +17,18 @@ def main():
     g.addEdge("e", "f",  8.0)
     g.addEdge("e", "g", 11.0)
     g.addEdge("f", "g", 10.0)
-    
-    # Obtenir un arbre couvrant de poids minimal du graphe
+
     tree = kruskal(g)
-    
-    # S'il existe un tel arbre (i.e., si le graphe est connexe)
+
+
     if tree != None:
-        
-        # L'afficher
         print(tree)
-    
     else:
         print("Pas d'arbre couvrant")
 
-# Applique l'algorithme de Kruskal pour trouver un arbre couvrant de poids minimal d'un graphe
-# Retourne: Un arbre couvrant de poids minimal du graphe ou None s'il n'en existe pas
+
+
+
 def kruskal(g):
     # Créer un nouveau graphe contenant les mêmes sommets que g
     tree = graph.Graph(g.nodes)
