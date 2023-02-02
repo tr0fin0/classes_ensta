77c40c1b9edbb59ec03ab874c98f2bd119ce07c8
tr0fin0 <guitrofino@gmail.com>
2023-02-02 08:31:15 +0100

    ✨ add exercice 4.2

--- 2A/RO202/TD/2023_01_26/sources/dijkstra.py
+++ 2A/RO202/TD/2023_01_26/sources/dijkstra.py
@@ -2,6 +2,9 @@ import graph
 import sys
 
 def main():
+    ex4_2()
+def ex4_2():
+    print("exercice 4.2)")
     cities = []
     cities.append("Paris")
     cities.append("Hambourg")
@@ -10,32 +13,40 @@ def main():
     cities.append("Edimbourg")
     cities.append("Berlin")
     cities.append("Stockholm")
-    cities.append("Rana")
     cities.append("Oslo")
+    cities.append("Rana")
 
     g = graph.Graph(cities)
-    
+
     g.addArc("Paris", "Hambourg", 7)
-    g.addArc("Paris",  "Londres", 4)
-    g.addArc("Paris",  "Amsterdam", 3)
-    g.addArc("Hambourg",  "Stockholm", 1)
-    g.addArc("Hambourg",  "Berlin", 1)
-    g.addArc("Londres",  "Edimbourg", 2)
-    g.addArc("Amsterdam",  "Hambourg", 2)
-    g.addArc("Amsterdam",  "Oslo", 8)
-    g.addArc("Stockholm",  "Oslo", 2)
-    g.addArc("Stockholm",  "Rana", 5)
-    g.addArc("Berlin",  "Amsterdam", 2)
-    g.addArc("Berlin",  "Stockholm", 1)
-    g.addArc("Berlin",  "Oslo", 3)
-    g.addArc("Edimbourg",  "Oslo", 7)
-    g.addArc("Edimbourg",  "Amsterdam", 3)
-    g.addArc("Edimbourg",  "Rana", 6)
-    g.addArc("Oslo",  "Rana", 2)
-    
-    # Applique l'algorithme de Dijkstra pour obtenir une arborescence
+    g.addArc("Paris", "Londres", 4)
+    g.addArc("Paris", "Amsterdam", 3)
+    g.addArc("Hambourg", "Stockholm", 1)
+    g.addArc("Hambourg", "Berlin", 1)
+    g.addArc("Londres", "Edimbourg", 2)
+    g.addArc("Amsterdam", "Hambourg", 2)
+    g.addArc("Amsterdam", "Oslo", 8)
+    g.addArc("Amsterdam", "Londres", 1)
+    g.addArc("Stockholm", "Oslo", 2)
+    g.addArc("Stockholm", "Rana", 5)
+    g.addArc("Berlin", "Amsterdam", 2)
+    g.addArc("Berlin", "Stockholm", 1)
+    g.addArc("Berlin", "Oslo", 3)
+    g.addArc("Edimbourg", "Oslo", 7)
+    g.addArc("Edimbourg", "Amsterdam", 3)
+    g.addArc("Edimbourg", "Rana", 6)
+    g.addArc("Oslo", "Rana", 2)
+
     tree = dijkstra(g, "Paris")
-    print(tree)
+    # tree = dijkstra(g, "Amsterdam")
+
+    if tree != None:
+        # print(tree)
+        print(repr(tree))
+    else:
+        print("no possible tree")
+
+
 
 def dijkstra(g, origin):
 		
