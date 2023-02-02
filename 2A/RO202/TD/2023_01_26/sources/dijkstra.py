03fd9dad24201cb82302c69fbe89222504d1bf0e
tr0fin0 <guitrofino@gmail.com>
2023-02-02 08:31:42 +0100

    ✨ add exercice 4.3

--- 2A/RO202/TD/2023_01_26/sources/dijkstra.py
+++ 2A/RO202/TD/2023_01_26/sources/dijkstra.py
@@ -3,6 +3,7 @@ import sys
 
 def main():
     ex4_2()
+    ex4_3()
 def ex4_2():
     print("exercice 4.2)")
     cities = []
@@ -48,10 +49,76 @@ def ex4_2():
 
 
 
-def dijkstra(g, origin):
-		
-   # Get the index of the origin 
-   r = g.indexOf(origin)
+def ex4_3():
+    print("exercice 4.3)")
+    cities = []
+    cities.append("a")
+    cities.append("b")
+    cities.append("c")
+    cities.append("d")
+    cities.append("e")
+    cities.append("f")
+    cities.append("g")
+    cities.append("r")
+
+    g = graph.Graph(cities)
+
+    g.addArc("r", "a", 5)
+    g.addArc("r", "b", 4)
+    g.addArc("b", "a", 5)
+    g.addArc("b", "c", 3)
+    g.addArc("b", "g", 9)
+    g.addArc("a", "c", 3)
+    g.addArc("d", "a", 8)
+    g.addArc("d", "e", 2)
+    g.addArc("e", "c", 4)
+    g.addArc("c", "d", 2)
+    g.addArc("c", "f", 6)
+    g.addArc("c", "g", 8)
+    g.addArc("g", "f", 5)
+
+    tree = dijkstra(g, "r")
+    # tree = dijkstra(g, "Amsterdam")
+
+    if tree != None:
+        # print(tree)
+        print(repr(tree))
+    else:
+        print("no possible tree")
+
+
+    cities = []
+    cities.append("A")
+    cities.append("B")
+    cities.append("C")
+    cities.append("D")
+    cities.append("E")
+    cities.append("F")
+    cities.append("G")
+    cities.append("r")
+
+    g = graph.Graph(cities)
+
+    g.addArc("r", "A", 2)
+    g.addArc("r", "G", 3)
+    g.addArc("A", "B", 3)
+    g.addArc("A", "F", 1)
+    g.addArc("G", "E", 2)
+    g.addArc("F", "G", 3)
+    g.addArc("F", "D", 4)
+    g.addArc("B", "C", 2)
+    g.addArc("E", "F", 2)
+    g.addArc("E", "D", 3)
+    g.addArc("D", "C", 2)
+
+    tree = dijkstra(g, "r")
+    # tree = dijkstra(g, "Amsterdam")
+
+    if tree != None:
+        # print(tree)
+        print(repr(tree))
+    else:
+        print("no possible tree")
 
    # Next node considered 
    pivot = r
