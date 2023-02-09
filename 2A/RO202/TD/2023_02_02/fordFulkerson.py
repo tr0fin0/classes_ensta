185edc7501f52da809af655c3b5384c8572f60b6
tr0fin0 <guitrofino@gmail.com>
2023-02-09 10:09:39 +0100

    💩 add fordFulkerson algorithm

--- 2A/RO202/TD/2023_02_02/fordFulkerson.py
+++ 2A/RO202/TD/2023_02_02/fordFulkerson.py
@@ -28,143 +28,117 @@ def example():
     g.addArc("d", "b", 2)
     g.addArc("d", "t", 6)
     g.addArc("e", "b", 4)
-    g.addArc("e", "t", 2)
-    
+    g.addArc("e", "d", 2)
+
     return g
 
-# Algorithme of Ford-Fulkerson
-def fordFulkerson(inputGraph: graph, startNode: str = "s", finishNode: str = "t") -> graph:
-    """
-    Marquage des sommets du graphe:
-     - mark[i] est égal à +j si le sommet d'indice i peut être atteint en augmentant le flot sur l'arc ji
-     - mark[i] est égal à -j si le sommet d'indice i peut être atteint en diminuant  le flot de  l'arc ji
-     - mark[i] est égal à sys.float_info.max si le sommet n'est pas marqué
-    """
-    nNodes = inputGraph.n
-
-    pred   = -1 # -1: no predecessor
-                #  i: predecessor i
-    signal = +0 # -1: flow to be decrease 
-                #  0: no change on flow
-                # +1: flow to be increase
-
-    # mark = [pred, signal]
-    preds  = [pred]   * nNodes
-    status = [signal] * nNodes
-    weights = [0] * nNodes
-
-    # marks = np.zeros((nNodes, 2))
-
-    # marks = []
-    # for i in range(nNodes):
-    #     marks.append(mark)
-    # marks = [mark] * nNodes
-    # print(f'mark:  {mark}')
-    # print(f'marks: {marks}')
-    # marked = mark
-    # print(marks)
-    print(preds)
-    print(status)
-
-    # Récupérer l'indice de la source et du puits
-    startIndex  = inputGraph.indexOf(startNode)
-    finishIndex = inputGraph.indexOf(finishNode)
-    # print(f'startIndex: {startIndex}, finishIndex: {finishIndex}')
-
-    # Créer un nouveau graphe contenant les même sommets que g
-    flow = graph.Graph(inputGraph.nodes)
-    # print(f'flow: {flow}')
-
-    # Récupérer tous les arcs du graphe 
-    arcs = inputGraph.getArcs()
-    # print(f'arcs: {arcs}')
-
-    # for arc in arcs:
-    #     # flow.addArcByIndex(arc.id1, arc.id2, arc.weight)
-    #     flow.addArcByIndex(arc.id1, arc.id2, 0)
-    #     # flow.addCopyOfEdge(arc)
-
-    # print(repr(flow))
-    # print(flow.adjacency)
-    # print("flow")
-
-    # for i in range(len(marks)):
-    #     print(f'pred: {marks[i][0]} signal: {marks[i][1]}')
-        # print(f'{i}: {inputGraph.getNeighbors(i)}')
-
-    # # i = 0
-    # print(marks[finishIndex][1])
-    # print(signal)
-    # # print(finishIndex)
-    # # print(len(marks))
-    # print(marks[finishIndex][1] == signal)
-    while(status[finishIndex] == signal):
-        # print("begin 1 while")
-        # interation index
-        actualIndex = startIndex
 
 
-        # reset marks to initial state
-        # marks = [mark] * nNodes
-        # marks = []
-        # for i in range(nNodes):
-        #     marks.append(mark)
-        # print(marks)
-        # print(f'p: {preds[actualIndex]} s: {status[actualIndex]}')
-        
-        # set start index with +
-        status[startIndex] = +1
-        # marks[startIndex][0] = 1
-        # print(marks[startIndex][1])
-        # print(marks)
+def fordFulkerson(capability: graph, sNode: str = "s", tNode: str = "t") -> graph:
+
+    def getNextNodes(g: graph, index: int) -> list:
+        nextNodes = []
+
+        for i in range(g.n):
+            if g.adjacency[index][i] != 0:
+                nextNodes.append(i)
+
+        return nextNodes
+
+    def getPreviousNodes(g: graph, index: int) -> list:
+        previousNodes = []
+
+        for i in range(g.n):
+            if g.adjacency[i][index] != 0:
+                previousNodes.append(i)
+
+        return previousNodes
+
+    def getConnections(g: graph, index: int) -> dict:
+        connections = {}
+
+        for i in range(g.n):
+            for j in range(g.n):
+                if g.adjacency[i][j] != 0:
+                    if   index == j:
+                        connections[i] = -1
+                    elif index == i:
+                        connections[j] = +1
+
+        return connections
 
 
-        neighbors = inputGraph.getNeighbors(actualIndex)
-        # print(marks)
-        print(neighbors)
-        # print(status[finishIndex])
-        while neighbors and status[finishIndex] != +1:
-            
-            # print("begin 2 while")
-            nextIndex = neighbors[actualIndex]
-            # print(nextIndex)
-            isMarked_i = status[actualIndex] == +1
-            isMarked_j = status[nextIndex] != 0
+    def findNextNode(actualNode: int) -> int:
+        nextNodes = getNextNodes(capability, actualNode)
+        for node in nextNodes:
+            # print(f"{nextNodes}")
+            flowMax = capability.adjacency[actualNode][node]
+            flowActual = flow.adjacency[actualNode][node]
 
-            flowInt = flow.adjacency[actualIndex][nextIndex]
-            flowMax = inputGraph.adjacency[actualIndex][nextIndex]
+            isFlowMax = not(flowActual < flowMax)
+            isNodeVisited = status[node] != 0
 
-            isFlowNull = flowInt == 0
-            isFlowFull = flowInt < flowMax
+            if isFlowMax == False and isNodeVisited == False:
+                status[node] = +1
+                weights.append(+(flowMax-flowActual))   # how much to full flow
+                return node
 
-            # print(isMarked_i)
-            # print(isMarked_j)
-            # print(isFlowFull)
-            # if marks[actualIndex][1] == +1 and marks[nextIndex][1] == 0 and :
-            if isMarked_i and not isMarked_j and isFlowFull:
-                # print(f'+i actualIndex: {actualIndex} nextIndex: {nextIndex}')
-                status[nextIndex] = +1 # increase flow
-                flow.adjacency[actualIndex][nextIndex]
-                pred[nextIndex] = actualIndex
-                actualIndex = nextIndex
 
-            if not isMarked_i and isMarked_j and not isFlowNull:
-                # print(f'-j actualIndex: {actualIndex} nextIndex: {nextIndex}')
-                status[nextIndex] = -1 # increase flow
-                pred[nextIndex] = actualIndex
-                flow.adjacency[][]
-                actualIndex = nextIndex
+        previousNodes = getPreviousNodes(capability, actualNode)
+        for node in previousNodes:
+            flowMax = capability.adjacency[node][actualNode]
+            flowActual = flow.adjacency[node][actualNode]
 
-            print(status)
-                
+            isFlowMax = not(flowActual < flowMax)
+            isNodeVisited = status[node] != 0
+
+            if isFlowMax == False and isNodeVisited == False:
+                status[node] = -1
+                weights.append(+(flowActual))
+                return node
+
+        return -1
+
+
+    def updateFlow() -> None:
+        minFlow = min(weights)
+
+        for i in range(1, len(path)):
+            if   status[path[i]] == +1:    # direct flow
+                flow.adjacency[path[i-1]][path[i]] = flow.adjacency[path[i-1]][path[i]] + minFlow
+            elif status[path[i]] == -1:    # reverse flow
+                flow.adjacency[path[i]][path[i-1]] = flow.adjacency[path[i]][path[i-1]] - minFlow
+
+        return None
+
+
+    flow = graph.Graph(capability.nodes)   # declare empty graph to store flow
+
+    nNodes = capability.n
+    sIndex = capability.indexOf(sNode)
+    tIndex = capability.indexOf(tNode)
+
+    while True:
+        path   = []
+        weights= []
+        status = [0] * nNodes
+
+        path.append(sIndex)
+
+        nextNode = 0
+        status[sIndex] = +1 # node visited
+        while status[tIndex] == 0 and nextNode != -1:
+            nextNode = findNextNode(path[-1])
+
+            if nextNode != -1:
+                path.append(nextNode)
+            elif nextNode != tIndex:
+                continue
+
+        updateFlow()
+        if status[tIndex] == 0:
             break
-        # print("end 2 while")
 
-        break
-    # print("end 1 while")
-
-
-    
     return flow
 
 
