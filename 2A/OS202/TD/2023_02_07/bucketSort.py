7f2a6fc22ffe03e3a8c7637f3515dbf0930fe49f
tr0fin0 <guitrofino@gmail.com>
2023-02-12 19:56:11 +0100

    ⚡️ improve usability

--- 2A/OS202/TD/2023_02_07/bucketSort.py
+++ 2A/OS202/TD/2023_02_07/bucketSort.py
@@ -1,12 +1,13 @@
 import numpy as np
+import time
 
 
 
 
-def getRandomArray(size: int = 100, seed:int = 1) -> list:
+def getRandomArray(nProcess: int = 25, seed:int = 1) -> list:
     np.random.seed = seed
 
-    return np.random.rand(size)
+    return np.random.rand(nProcess)
 
 
 def printArrayInfo(array: list) -> None:
@@ -14,15 +15,61 @@ def printArrayInfo(array: list) -> None:
     print(f'mean: {np.mean(array):1.6f}')
     print(f'max:  {max(array):1.6f}')
 
+    printArray(array)
     return None
+
+
+def printArray(array: list) -> None:
+    for index in range(len(array)):
+        print(f'{index:3.0f}: {array[index]:1.6f}')
+
+    return None
+
+
+def bucketSort(array: list, numBuckets: int = 10) -> list:
+    buckets = []
+
+    # create buckets
+    for i in range(numBuckets):
+        buckets.append([])
+
+    # adding values to 
+    for value in array:
+        index = int(value * numBuckets)
+        buckets[index].append(value)
+
+    for index in range(len(buckets)):
+        buckets[index] = sorted(buckets[index])
+
+    sortedArray = []
+    for bucket in buckets:
+        for value in bucket:
+            sortedArray.append(value)
+
+    return sortedArray
+
 
 
 
 
 def main():
+    # initialization random array
+    array = getRandomArray(int(1e7))
+    # printArrayInfo(array)   # debug
 
-    array = getRandomArray()
-    printArrayInfo(array)
+
+    # bucket sort
+    start = time.time()
+    arraySorted = bucketSort(array)
+    end = time.time()
+    print(f'[{(end - start):2.6f} s]: serial')
+
+
+    # verification
+    # start = time.time()
+    # if (arraySorted == sorted(array)):
+    #     end = time.time()
+    #     print(f"[{(end - start):2.6f} s]: sorting successful")
 
 
 
