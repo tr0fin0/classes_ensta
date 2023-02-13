93a03f1726d258775e42e830039cf3dada37c602
tr0fin0 <guitrofino@gmail.com>
2023-02-13 08:00:20 +0100

    🧪 gather vectors in single array

--- 2A/OS202/TD/2023_02_07/bucketSortParallel.py
+++ 2A/OS202/TD/2023_02_07/bucketSortParallel.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 import time
 from mpi4py import MPI
@@ -7,10 +8,11 @@ from mpi4py import MPI
 
 def main():
     # initialization MPI
-    # mpirun -np 4 python3 bucketSort.py
+    # mpirun -np 10 python3 bucketSort.py
     comm = MPI.COMM_WORLD
     nProcess = comm.Get_size()
     process  = comm.Get_rank()
+    root = 0
 
     def getRandomArray(nProcess: int = 25, seed:int = 1) -> list:
         np.random.seed = seed
@@ -19,28 +21,52 @@ def main():
 
 
     def bucketSortParallel(arraySize: float = 1e1) -> list:
-        if process == 0:
+
+        if process == root:
             # main process:
             # generate data
             array = getRandomArray(int(arraySize))
             buckets = []
 
+        if process == root:
             # create buckets
             for i in range(nProcess):
+            # for i in range(nProcess - 1):
                 buckets.append([])
 
             # adding values
             for value in array:
-                index = int(value * nProcess)
+                index = int(value * (nProcess))
+                # index = int(value * (nProcess - 1))
                 buckets[index].append(value)
+
+
+            # sendcounts = np.array(comm.gather(len(sendbuf), root))
+            # for i in range(1, (nProcess+1)):
+            #     comm.send(buckets[i], dest=(i))
+            #     print(f'0: {i}')
+
+            # sortedArray = []
+            # recvbuf = np.empty(sum(sendcounts), dtype=float)
         else:
             buckets = None
+            # buckets = comm.recv(source=0)
+            # recvbuf = None
+            # sendbuf = np.array(buckets)
 
-        # each process receives a bucket and sort it
+        # if process == 0:
+        #     sortedArray = comm.gather(buckets, root=0)
+        # else:
+        #     buckets = sorted(buckets)
+
+        # # each process receives a bucket and sort it
         buckets = sorted(comm.scatter(buckets, 0))
 
-        # each process sends it's sorted bucket to main process
-        sortedArray = comm.gather(buckets, root=0)
+        # # each process sends it's sorted bucket to main process
+        sortedArray = comm.gather(buckets, root=root)
+
+
+        # comm.Gatherv(sendbuf=sendbuf, recvbuf=(recvbuf, sendcounts), root=root)
 
         return sortedArray
 
