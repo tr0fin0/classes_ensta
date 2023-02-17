daea1152b5c1e4feb7d391d1eb008bdb9612b25d
tr0fin0 <guitrofino@gmail.com>
2023-02-17 11:53:23 +0100

    ♻️ create functions

--- 2A/MI204/TP/2023_02_10/TP1_Features/Convolutions.py
+++ 2A/MI204/TP/2023_02_10/TP1_Features/Convolutions.py
@@ -1,83 +1,112 @@
 import numpy as np
 import cv2
-# from google.colab.patches import cv2_imshow
-
 from matplotlib import pyplot as plt
 
 # 2A/MI204/TP/2023_02_10/TP1_Features
 
-#Lecture image en niveau de gris et conversion en float64
-# print("check")
-img=np.float64(cv2.imread('../Image_Pairs/FlowerGarden2.png',0))
-# print("check")
-(h, w) = img.shape
-print(f"image : {h} x {w} pixels")
-plt.imshow(img,cmap = 'gray')
-plt.title('Image Raw')
-plt.show()
 
 
-#Méthode directe
-t1 = cv2.getTickCount()
-img2 = cv2.copyMakeBorder(img,0,0,0,0,cv2.BORDER_REPLICATE)
-for y in range(1,h-1):
+
+def plotImg(image, title: str = 'title', vmin: float = -1.0, vmax: float = -1.0) -> None:
+  """
+  plotImg():
+
+  """
+
+  if vmin == -1 or vmax == -1:
+    plt.imshow(image, cmap = 'gray')
+  else:
+    plt.imshow(image, cmap = 'gray', vmin = vmin, vmax = vmax)
+
+  plt.title(title)
+  plt.show()
+  
+  return None
+
+
+def methodDiscrete(image) -> np.float64:
+  """
+  methodDiscrete():
+
+  """
+
+  #Méthode directe
+  start = cv2.getTickCount()
+  img = cv2.copyMakeBorder(image,0,0,0,0,cv2.BORDER_REPLICATE)
+  (h, w) = img.shape
+
   for x in range(1,w-1):
-    # kernel:
-    #    -1
-    # -1 +5 -1
-    #    -1
+    for y in range(1,h-1):
+      # kernel:
+      #    -1
+      # -1 +5 -1
+      #    -1
 
-    # val = +5*img[y, x] - img[y-1, x] - img[y, x-1] - img[y+1, x] - img[y, x+1] 
-    val = -5*img[y, x] + img[y-1, x] + img[y, x-1] + img[y+1, x] + img[y, x+1] 
-    img2[y,x] = min(max(val,0),255)
-t2 = cv2.getTickCount()
-time = (t2 - t1)/ cv2.getTickFrequency()
-print(f"Méthode Directe : {time:1.8f} s")
+      val = -5*image[y, x] + image[y-1, x] + image[y, x-1] + image[y+1, x] + image[y, x+1] 
+      img[y,x] = min(max(val,0),255)
+  end = cv2.getTickCount()
 
-# cv2.imshow did not work. gave problems
-# cv2.imshow('Avec boucle python',img2.astype(np.uint8))
-# # cv2_imshow(img2.astype(np.uint8))
-# #Convention OpenCV : une image de type entier est interprétée dans {0,...,255}
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-# print("check")
+  time = (start - end)/ cv2.getTickFrequency()
+  print(f"Méthode Directe : {time:1.8f} s")
 
-plt.subplot(121)
-plt.imshow(img2,cmap = 'gray')
-plt.title('Convolution - Méthode Directe')
+  plotImg(img, 'Convolution - Méthode Directe')
 
-#Méthode filter2D
-t1 = cv2.getTickCount()
-kernel = np.array([[0, -1, 0],[-1, 5, -1],[0, -1, 0]])
-img3 = cv2.filter2D(img,-1,kernel)
-t2 = cv2.getTickCount()
-time = (t2 - t1)/ cv2.getTickFrequency()
-print(f"Méthode Filter2D: {time:1.8f} s")
-
-# cv2.imshow('Avec filter2D',img3/255.0)
-# #Convention OpenCV : une image de type flottant est interprétée dans [0,1]
-# cv2.waitKey()
-
-plt.subplot(122)
-plt.imshow(img3,cmap = 'gray',vmin = 0.0,vmax = 255.0)
-#Convention Matplotlib : par défaut, normalise l'histogramme !
-plt.title('Convolution - filter2D')
-plt.show()
+  return img
 
 
-# Q1
-#   comment on peut voir avec le temps d'exécution, l'OpenCV est beaucoup plus efficace.
+def methodOpenCV(image) -> np.float64:
+  """
+  methodOpenCV()
 
-#   les fonctions pythons sont interpretes par contre que l'OpenCV est compile de la libraire du C++ que rende le code plus efficace.
+  """
 
-# Q2
-#   on voir que le kernel propose est:
-#     kernel:
-#     +0 -1 +0
-#     -1 +5 -1
-#     +0 -1 +0
-#   le rehaussement de contraste arrive...
-#   f-ftt equations from the board you took from the last class need the math proof
+  #Méthode filter2D
+  start = cv2.getTickCount()
+  kernel = np.array([[0, -1, 0],[-1, 5, -1],[0, -1, 0]])
+  img = cv2.filter2D(image, -1, kernel)
+  end = cv2.getTickCount()
 
-# Q3
-#   see poly
+  time = (start - end)/ cv2.getTickFrequency()
+  print(f"Méthode Filter2D: {time:1.8f} s")
+  # il faut normalizer l'image
+  plotImg(image, 'Convolution - filter2D', 0.0, 255.0)
+
+  return img
+
+
+
+def main() -> None:
+  image = np.float64(cv2.imread('../Image_Pairs/FlowerGarden2.png',0))
+
+  plotImg(image, 'image original')
+
+  imageDiscrete = methodDiscrete(image)
+  imageOpenCV = methodOpenCV(image)
+
+  return None
+
+
+
+
+
+
+if __name__ == "__main__":
+  main()
+
+
+  # Q1
+  #   comment on peut voir avec le temps d'exécution, l'OpenCV est beaucoup plus efficace.
+
+  #   les fonctions pythons sont interpretes par contre que l'OpenCV est compile de la libraire du C++ que rende le code plus efficace.
+
+  # Q2
+  #   on voir que le kernel propose est:
+  #     kernel:
+  #     +0 -1 +0
+  #     -1 +5 -1
+  #     +0 -1 +0
+  #   le rehaussement de contraste arrive...
+  #   f-ftt equations from the board you took from the last class need the math proof
+
+  # Q3
+  #   see poly
