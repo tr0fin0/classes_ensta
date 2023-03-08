418643a263c47e7929b73fce7ed3a6db88164d37
tr0fin0 <guitrofino@gmail.com>
2023-03-08 06:07:18 +0100

    ✨ add Q8

--- 2A/MI204/TP/2023_02_10/TP1_Features/Features_Detect.py
+++ 2A/MI204/TP/2023_02_10/TP1_Features/Features_Detect.py
@@ -1,107 +1,351 @@
-import numpy as np
+import sys
 import cv2
-
+import numpy as np
 from matplotlib import pyplot as plt
 
-import sys
-if len(sys.argv) != 2:
-  print ("Usage :",sys.argv[0],"detector(= orb ou kaze)")
-  sys.exit(2)
-if sys.argv[1].lower() == "orb":
-  detector = 1
-elif sys.argv[1].lower() == "kaze":
-  detector = 2
-else:
-  print ("Usage :",sys.argv[0],"detector(= orb ou kaze)")
-  sys.exit(2)
-
-#Lecture de la paire d'images
-img1 = cv2.imread('../Image_Pairs/torb_small1.png')
-print("Dimension de l'image 1 :",img1.shape[0],"lignes x",img1.shape[1],"colonnes")
-print("Type de l'image 1 :",img1.dtype)
-img2 = cv2.imread('../Image_Pairs/torb_small2.png')
-print("Dimension de l'image 2 :",img2.shape[0],"lignes x",img2.shape[1],"colonnes")
-print("Type de l'image 2 :",img2.dtype)
-
-#Début du calcul
-t1 = cv2.getTickCount()
-#Création des objets "keypoints"
-if detector == 1:
-  kp1 = cv2.ORB_create(nfeatures = 250,#Par défaut : 500
-                       scaleFactor = 2,#Par défaut : 1.2
-                       nlevels = 3)#Par défaut : 8
-  kp2 = cv2.ORB_create(nfeatures=250,
-                        scaleFactor = 2,
-                        nlevels = 3)
-  print("Détecteur : ORB")
-else:
-  kp1 = cv2.KAZE_create(upright = False,#Par défaut : false
-    		        threshold = 0.001,#Par défaut : 0.001
-  		        nOctaves = 4,#Par défaut : 4
-		        nOctaveLayers = 4,#Par défaut : 4
-		        diffusivity = 2)#Par défaut : 2
-  kp2 = cv2.KAZE_create(upright = False,#Par défaut : false
-	  	        threshold = 0.001,#Par défaut : 0.001
-		        nOctaves = 4,#Par défaut : 4
-		        nOctaveLayers = 4,#Par défaut : 4
-		        diffusivity = 2)#Par défaut : 2
-  print("Détecteur : KAZE")
-#Conversion en niveau de gris
-gray1 =  cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
-gray2 =  cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-#Détection des keypoints
-pts1 = kp1.detect(gray1,None)
-pts2 = kp2.detect(gray2,None)
-t2 = cv2.getTickCount()
-time = (t2 - t1)/ cv2.getTickFrequency()
-print("Détection des points d'intérêt :",time,"s")
-
-#Affichage des keypoints
-img1 = cv2.drawKeypoints(gray1, pts1, None, flags=4)
-# flags définit le niveau d'information sur les points d'intérêt
-# 0 : position seule ; 4 : position + échelle + direction
-img2 = cv2.drawKeypoints(gray2, pts2, None, flags=4)
-
-plt.subplot(121)
-plt.imshow(img1)
-plt.title('Image n°1')
-
-plt.subplot(122)
-plt.imshow(img2)
-plt.title('Image n°2')
-
-plt.show()
 
 
-# detection de harris 
-# page 28 2_caracteristiques
+def selectAlgorithm() -> int:
+    if len(sys.argv) != 2:
+        print("Usage :", sys.argv[0], "detector(= orb ou kaze)")
+        sys.exit(2)
+    if sys.argv[1].lower() == "orb":
+        detector = 1
+    elif sys.argv[1].lower() == "kaze":
+        detector = 2
+    else:
+        print("Usage :", sys.argv[0], "detector(= orb ou kaze)")
+        sys.exit(2)
 
-# page 34 2_caracteristiques
-# EVALUATION DES DETECTEURS DE POINTS D’INTERET
-# La plupart des détecteurs de point d’intérêt sont définis indépendamment des descripteurs
-# avec lesquels on les utilise. Il est donc nécessaire de pouvoir les évaluer en eux-mêmes.
-# Les propriétés recherchées d’un bon détecteur :
-# • Répétabilité : le point doit
-# apparaître aux mêmes endroits
-# quelque soit la déformation.
-# • Représentativité : les points
-# doivent être le plus nombreux
-# possible.
-# • Efficacité : le détecteur doit être
-# rapide à calculer (cf SURF, FAST)
-# (Rq : répétabilité et représentativité ne
-# sont pas indépendants !)
-
-# documentation of the openCV library to better understand the different codes avaleables the video is an interpretation of the text
-#   https://docs.opencv.org/3.4/df/d54/tutorial_py_features_meaning.html
-#     https://www.youtube.com/watch?v=DZtUt4bKtmY
-# 
-
-# introduction of the facts with the wikipedia page to explain what is the features detection method
-#   https://en.wikipedia.org/wiki/Feature_(computer_vision)#Detectors
-
-# sobel kernels are the a kind of feature detection, an edge detection features
+    return detector
 
 
-# paper that explains in its abstract the differences in the code of the laze and in the orb explaining why it is beeter in some ways
-#   https://www.doc.ic.ac.uk/~ajd/Publications/alcantarilla_etal_eccv2012.pdf
+def Q6():
+    img1 = cv2.imread('../Image_Pairs/torb_small1.png')
+    img2 = cv2.imread('../Image_Pairs/torb_small2.png')
+
+    start = cv2.getTickCount()
+
+    nFeatures = 250
+    kp1_ORB = cv2.ORB_create(
+        nfeatures=nFeatures,  #Par défaut : 500
+        scaleFactor=2,        #Par défaut : 1.2
+        nlevels=3)            #Par défaut : 8
+    kp2_ORB = cv2.ORB_create(nfeatures=nFeatures, scaleFactor=2, nlevels=3)
+
+    kp1_KAZE = cv2.KAZE_create(
+        upright=False,    #Par défaut : false
+        threshold=0.001,  #Par défaut : 0.001
+        nOctaves=4,       #Par défaut : 4
+        nOctaveLayers=4,  #Par défaut : 4
+        diffusivity=2)    #Par défaut : 2
+    kp2_KAZE = cv2.KAZE_create(
+        upright=False,    #Par défaut : false
+        threshold=0.001,  #Par défaut : 0.001
+        nOctaves=4,       #Par défaut : 4
+        nOctaveLayers=4,  #Par défaut : 4
+        diffusivity=2)    #Par défaut : 2
+
+    #Conversion en niveau de gris
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    #Détection des keypoints
+    pts1_ORB = kp1_ORB.detect(gray1, None)
+    pts2_ORB = kp2_ORB.detect(gray2, None)
+
+    pts1_KAZE = kp1_KAZE.detect(gray1, None)
+    pts2_KAZE = kp2_KAZE.detect(gray2, None)
+    end = cv2.getTickCount()
+    time = (end - start) / cv2.getTickFrequency()
+
+
+    #Affichage des keypoints
+    img1_ORB = cv2.drawKeypoints(gray1, pts1_ORB, None, flags=4)
+    img2_ORB = cv2.drawKeypoints(gray2, pts2_ORB, None, flags=4)
+
+    img1_KAZE = cv2.drawKeypoints(gray1, pts1_KAZE, None, flags=4)
+    img2_KAZE = cv2.drawKeypoints(gray2, pts2_KAZE, None, flags=4)
+    # flags définit le niveau d'information sur les points d'intérêt
+    # 0 : position seule ; 4 : position + échelle + direction
+
+    plt.subplot(221)
+    plt.imshow(img1_ORB)
+    plt.title(f'image 1, ORB')
+
+    plt.subplot(222)
+    plt.imshow(img2_ORB)
+    plt.title(f'image 2, ORB')
+
+    plt.subplot(223)
+    plt.imshow(img1_KAZE)
+    plt.title(f'image 1, KAZE')
+
+    plt.subplot(224)
+    plt.imshow(img2_KAZE)
+    plt.title(f'image 2, KAZE')
+
+    plt.savefig(f'./images/Q6A.svg')
+    # plt.set_xticks([])
+    # plt.set_yticks([])
+    plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
+    plt.show()
+
+
+
+
+def Q8():
+    def crossCheck():
+        img1 = cv2.imread('../Image_Pairs/torb_small1.png')
+        img2 = cv2.imread('../Image_Pairs/torb_small2.png')
+
+        start = cv2.getTickCount()
+
+        nFeatures = 250
+        kp1_ORB = cv2.ORB_create(nfeatures=nFeatures, scaleFactor=2, nlevels=3)
+        kp2_ORB = cv2.ORB_create(nfeatures=nFeatures, scaleFactor=2, nlevels=3)
+
+        kp1_KAZE = cv2.KAZE_create(
+            upright=False,    #Par défaut : false
+            threshold=0.001,  #Par défaut : 0.001
+            nOctaves=4,       #Par défaut : 4
+            nOctaveLayers=4,  #Par défaut : 4
+            diffusivity=2)    #Par défaut : 2
+        kp2_KAZE = cv2.KAZE_create(
+            upright=False,    #Par défaut : false
+            threshold=0.001,  #Par défaut : 0.001
+            nOctaves=4,       #Par défaut : 4
+            nOctaveLayers=4,  #Par défaut : 4
+            diffusivity=2)    #Par défaut : 2
+
+        #Conversion en niveau de gris
+        gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+        #Détection et description des keypoints
+        pts1_ORB, desc1_ORB = kp1_ORB.detectAndCompute(gray1, None)
+        pts2_ORB, desc2_ORB = kp2_ORB.detectAndCompute(gray2, None)
+
+        pts1_KAZE, desc1_KAZE = kp1_KAZE.detectAndCompute(gray1, None)
+        pts2_KAZE, desc2_KAZE = kp2_KAZE.detectAndCompute(gray2, None)
+
+        #Les points non appariés apparaîtront en gris
+        img1_ORB = cv2.drawKeypoints(gray1, pts1_ORB, None, color=(127, 127, 127), flags=0)
+        img2_ORB = cv2.drawKeypoints(gray2, pts2_ORB, None, color=(127, 127, 127), flags=0)
+
+        img1_KAZE = cv2.drawKeypoints(gray1, pts1_KAZE, None, color=(127, 127, 127), flags=0)
+        img2_KAZE = cv2.drawKeypoints(gray2, pts2_KAZE, None, color=(127, 127, 127), flags=0)
+
+        # Calcul de l'appariement
+        start = cv2.getTickCount()
+        bf_ORB  = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        bf_KAZE = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+
+        matches_ORB = bf_ORB.match(desc1_ORB, desc2_ORB)
+        matches_ORB = sorted(matches_ORB, key=lambda x: x.distance)
+
+        matches_KAZE = bf_KAZE.match(desc1_KAZE, desc2_KAZE)
+        matches_KAZE = sorted(matches_KAZE, key=lambda x: x.distance)
+
+        end = cv2.getTickCount()
+        time = (end - start) / cv2.getTickFrequency()
+        print(f"{time} s")
+
+        # Trace les N meilleurs appariements
+        Nbest = 200
+        img3_ORB = cv2.drawMatches(img1_ORB, pts1_ORB, img2_ORB, pts2_ORB, matches_ORB[:Nbest], None, flags=2)
+        img3_KAZE = cv2.drawMatches(img1_KAZE, pts1_KAZE, img2_KAZE, pts2_KAZE, matches_KAZE[:Nbest], None, flags=2)
+
+        plt.subplot(121)
+        plt.imshow(img3_ORB)
+        plt.title(f'Cross Check {Nbest}, ORB')
+
+        plt.subplot(122)
+        plt.imshow(img3_KAZE)
+        plt.title(f'Cross Check {Nbest}, KAZE')
+
+        plt.savefig(f'./images/Q8CC.svg')
+        plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
+        plt.show()
+
+
+    def FLANN():
+        img1 = cv2.imread('../Image_Pairs/torb_small1.png')
+        img2 = cv2.imread('../Image_Pairs/torb_small2.png')
+
+        start = cv2.getTickCount()
+
+        nFeatures = 250
+        kp1_ORB = cv2.ORB_create(nfeatures=nFeatures, scaleFactor=2, nlevels=3)
+        kp2_ORB = cv2.ORB_create(nfeatures=nFeatures, scaleFactor=2, nlevels=3)
+
+        kp1_KAZE = cv2.KAZE_create(
+            upright=False,    #Par défaut : false
+            threshold=0.001,  #Par défaut : 0.001
+            nOctaves=4,       #Par défaut : 4
+            nOctaveLayers=4,  #Par défaut : 4
+            diffusivity=2)    #Par défaut : 2
+        kp2_KAZE = cv2.KAZE_create(
+            upright=False,    #Par défaut : false
+            threshold=0.001,  #Par défaut : 0.001
+            nOctaves=4,       #Par défaut : 4
+            nOctaveLayers=4,  #Par défaut : 4
+            diffusivity=2)    #Par défaut : 2
+
+        #Conversion en niveau de gris
+        gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+        #Détection et description des keypoints
+        pts1_ORB, desc1_ORB = kp1_ORB.detectAndCompute(gray1, None)
+        pts2_ORB, desc2_ORB = kp2_ORB.detectAndCompute(gray2, None)
+
+        desc1_ORB = np.float64(desc1_ORB)
+        desc2_ORB = np.float64(desc2_ORB)
+        # if(desc1_ORB.dtype()!=CV_32F):
+        #     desc1_ORB.convertTo(desc1_ORB, CV_32F)
+
+        # if(desc2_ORB.dtype()!=CV_32F):
+        #     desc2_ORB.convertTo(desc2_ORB, CV_32F)
+
+
+        pts1_KAZE, desc1_KAZE = kp1_KAZE.detectAndCompute(gray1, None)
+        pts2_KAZE, desc2_KAZE = kp2_KAZE.detectAndCompute(gray2, None)
+
+        # Calcul de l'appariement
+        start = cv2.getTickCount()
+        FLANN_INDEX_KDTREE = 0
+        index_params  = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks=50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        matches_ORB  = flann.knnMatch(desc1_ORB, desc2_ORB, k=2)
+        matches_KAZE = flann.knnMatch(desc1_KAZE, desc2_KAZE, k=2)
+
+        # Application du ratio test
+        good_ORB = []
+        for m,n in matches_ORB:
+          if m.distance < 0.7*n.distance:
+            good_ORB.append([m])
+
+        good_KAZE = []
+        for m,n in matches_KAZE:
+          if m.distance < 0.7*n.distance:
+            good_KAZE.append([m])
+
+        draw_params = dict(matchColor = (0,255,0),
+                          singlePointColor = (255,0,0),
+                          flags = 0)
+
+        # Affichage des appariements qui respectent le ratio test
+        img3_ORB = cv2.drawMatchesKnn(gray1, pts1_ORB, gray2, pts2_ORB, good_ORB, None, **draw_params)
+        img3_KAZE = cv2.drawMatchesKnn(gray1, pts1_KAZE, gray2, pts2_KAZE, good_KAZE, None, **draw_params)
+
+        end = cv2.getTickCount()
+        time = (end - start) / cv2.getTickFrequency()
+        print(f"{time} s")
+
+        plt.subplot(121)
+        plt.imshow(img3_ORB)
+        plt.title(f'FLANN {len(good_ORB)}, ORB')
+
+        plt.subplot(122)
+        plt.imshow(img3_KAZE)
+        plt.title(f'FLANN {len(good_KAZE)}, KAZE')
+
+        plt.savefig(f'./images/Q8FL.svg')
+        plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
+        plt.show()
+
+
+    def RATIO():
+        img1 = cv2.imread('../Image_Pairs/torb_small1.png')
+        img2 = cv2.imread('../Image_Pairs/torb_small2.png')
+
+        nFeatures = 250
+        kp1_ORB = cv2.ORB_create(nfeatures=nFeatures, scaleFactor=2, nlevels=3)
+        kp2_ORB = cv2.ORB_create(nfeatures=nFeatures, scaleFactor=2, nlevels=3)
+
+        kp1_KAZE = cv2.KAZE_create(
+            upright=False,    #Par défaut : false
+            threshold=0.001,  #Par défaut : 0.001
+            nOctaves=4,       #Par défaut : 4
+            nOctaveLayers=4,  #Par défaut : 4
+            diffusivity=2)    #Par défaut : 2
+        kp2_KAZE = cv2.KAZE_create(
+            upright=False,    #Par défaut : false
+            threshold=0.001,  #Par défaut : 0.001
+            nOctaves=4,       #Par défaut : 4
+            nOctaveLayers=4,  #Par défaut : 4
+            diffusivity=2)    #Par défaut : 2
+
+        gray1 =  cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        gray2 =  cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+        pts1_ORB, desc1_ORB = kp1_ORB.detectAndCompute(gray1, None)
+        pts2_ORB, desc2_ORB = kp2_ORB.detectAndCompute(gray2, None)
+
+        pts1_KAZE, desc1_KAZE = kp1_KAZE.detectAndCompute(gray1, None)
+        pts2_KAZE, desc2_KAZE = kp2_KAZE.detectAndCompute(gray2, None)
+
+
+        start = cv2.getTickCount()
+
+        bf_ORB  = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+        bf_KAZE = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+
+        matches_ORB = bf_ORB.knnMatch(desc1_ORB, desc2_ORB, k=2)
+        matches_KAZE = bf_KAZE.knnMatch(desc1_KAZE, desc2_KAZE, k=2)
+
+        good_ORB = []
+        for m,n in matches_ORB:
+            if m.distance < 0.7*n.distance:
+                good_ORB.append([m])
+
+        good_KAZE = []
+        for m,n in matches_KAZE:
+            if m.distance < 0.7*n.distance:
+                good_KAZE.append([m])
+
+
+        end = cv2.getTickCount()
+        time = (end - start)/ cv2.getTickFrequency()
+
+        # Affichage des appariements qui respectent le ratio test
+        draw_params = dict(matchColor = (0,255,0),
+                        singlePointColor = (255,0,0),
+                        flags = 0)
+        img3_ORB = cv2.drawMatchesKnn(gray1, pts1_ORB, gray2, pts2_ORB, good_ORB, None, **draw_params)
+        img3_KAZE = cv2.drawMatchesKnn(gray1, pts1_KAZE, gray2, pts2_KAZE, good_KAZE, None, **draw_params)
+
+        plt.subplot(121)
+        plt.imshow(img3_ORB)
+        plt.title(f'Ratio Test {len(good_ORB)}, ORB')
+
+        plt.subplot(122)
+        plt.imshow(img3_KAZE)
+        plt.title(f'Ratio Test {len(good_KAZE)}, KAZE')
+
+        plt.savefig(f'./images/Q8RT.svg')
+        plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
+        plt.show()
+
+
+    crossCheck()
+    # FLANN()
+    # RATIO()
+
+
+
+
+def main():
+    # Q6()
+    Q8()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
