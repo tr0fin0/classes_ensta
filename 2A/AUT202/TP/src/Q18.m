ace87e6a66718abbb561e3952dec2547a63cb970
tr0fin0 <guitrofino@gmail.com>
2023-03-12 16:15:10 +0100

    ✨ add Q18 code

---
+++ 2A/AUT202/TP/src/Q18.m
@@ -0,0 +1,9 @@
+function [x10h, x20h, x30h, x40h] = Q18()
+    %   Initial Conditions Different from Q6
+    %   otherwise the System and Observator 
+    %   would be the same
+    x10h= 0.01;    % inicial value of x1 (zero)
+    x20h= 0.01;    % inicial value of x2 (zero)
+    x30h= 0.01;    % inicial value of x3 (zero)
+    x40h= 0.01;    % inicial value of x4 (zero)
+end
