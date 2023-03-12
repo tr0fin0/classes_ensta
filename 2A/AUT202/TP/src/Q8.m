238f67a9d02698e564780a42e12c8c3b61bd75e4
tr0fin0 <guitrofino@gmail.com>
2023-03-12 16:14:37 +0100

    ✨ add Q8 code

---
+++ 2A/AUT202/TP/src/Q8.m
@@ -0,0 +1,7 @@
+function [s, rz, rref] = Q8()    
+    s   = 0;    % step of input function
+    rz  = 0;    % inicial value of r (zero)
+    rref= 0;    %   final value of r
+    
+    disp(['setup: reference distance ' num2str(rref)]);
+end
