97d493183cc0e17fcf2ad6e395b98111e4825860
tr0fin0 <guitrofino@gmail.com>
2023-03-12 16:13:59 +0100

    ✨ add part 3 variation code

---
+++ 2A/AUT202/TP/src/P30.m
@@ -0,0 +1,32 @@
+function [I1, I2, A, B, C, Ko, Kw] = P30(x10, x20, x30, x40, w)
+    % initial variables
+    I1 = [x10, x20, x30, x40];
+    I2 = [0,0,0,0];
+    
+    % matrixes
+    [J, m, sig, g] = Q5();
+    A   = [...
+        0       1 0             0;...
+        0       0 -(g/(1+sig))  0;...
+        0       0 0             1;...
+        -m*g/J  0 0             0;
+    ];
+
+    B   = [
+        0;
+        0;
+        0;
+        1/J;
+    ];
+
+    C   = [
+        1 0 0 0;
+        0 0 1 0;
+    ];
+
+    
+    Q = eye(4); 
+    R = 1;
+    [Ko, S, CLP] = Q15(A, B, Q, R);
+    Kw = Q12(A, B, w);   
+end
