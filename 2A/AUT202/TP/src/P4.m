e1896e69246c39d1eebba55fac4c4e293ff9b568
tr0fin0 <guitrofino@gmail.com>
2023-03-12 16:13:28 +0100

    ✨ add part 4 code

---
+++ 2A/AUT202/TP/src/P4.m
@@ -0,0 +1,37 @@
+function [I1, I2, A, B, C, K, L] = P4(x10, x20, x30, x40, x10h, x20h, x30h, x40h, w)
+    % initial variables
+    I1 = [x10, x20, x30, x40];
+    I2 = [x10h, x20h, x30h, x40h];
+    
+    % matrixes
+    [J, m, sig, g] = Q5();
+    A   = [...
+        0       1 0             0;...
+        0       0 -(g/(1+sig))  0;...
+        0       0 0             1;...
+        -m*g/J  0 0             0;
+    ];
+
+    B   = [
+        0;
+        0;
+        0;
+        1/J;
+    ];
+
+    C   = [
+        1 0 0 0;
+        0 0 1 0;
+    ];
+
+    
+    if w == inf
+        Q = eye(4); 
+        R = 1;
+        [K, S, CLP] = Q15(A, B, Q, R);
+    else
+        K = Q12(A, B, w);
+    end    
+    
+    L = Q21(A, C, w);
+end
