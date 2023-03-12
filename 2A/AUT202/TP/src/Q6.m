023dfe257ebb494f09f88c01c17511c13f7b2d31
tr0fin0 <guitrofino@gmail.com>
2023-03-12 16:14:29 +0100

    ✨ add Q6 code

---
+++ 2A/AUT202/TP/src/Q6.m
@@ -0,0 +1,6 @@
+function [x10, x20, x30, x40] = Q6()    
+    x10 = 0.02;    % inicial value of x1 (zero)
+    x20 = 0.02;    % inicial value of x2 (zero)
+    x30 = 0.02;    % inicial value of x3 (zero)
+    x40 = 0.02;    % inicial value of x4 (zero)
+end
