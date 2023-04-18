6fd951fe6dc7ae0cc46505c6729c6e9b6d7fdc12
tr0fin0 <guitrofino@gmail.com>
2023-04-18 09:13:57 +0200

    ✨ add TD MATLAB file

---
+++ 2A/MI210/TP/2023_04_18/masters_course_reduced.m
@@ -0,0 +1,146 @@
+%% Exercise 1: compute entropy of 2 neurons, with varying covariance matrices  %%%%%%%
+
+
+
+%%  Load data
+
+load('data.mat')
+
+% x: 16000 image patches of size 12 x 12 = 144
+% X: 1 full image 
+% 
+% WZ: decorrelating filters
+% WI: ICA filters
+
+%% Exercise 2
+
+% compute the covariance of x
+% C = ...
+
+% compute z = WZ*x, and its covariance
+% z = ...
+% Czz = ...
+
+figure('Name', 'covariance')
+subplot(1, 2, 1)
+imagesc(C(1:12, 1:12)); 
+colormap('gray')
+title('stimulus covariance')
+subplot(1, 2, 2)
+imagesc(Czz(1:12, 1:12)); 
+colormap('gray')
+title('covariance of Z')
+
+%% Exercise 3
+
+% reshape 1 filter
+w = reshape(WZ(78, :), 12, 12);
+
+Z = conv2(X, w, 'same');
+
+figure('Name', 'images')
+subplot(2, 2, 1)
+imagesc(X);
+subplot(2, 2, 2)
+imagesc(Z, [-1 1]);
+
+
+%%%%% TODO %%%%%%%%%%%%%%%%%
+eta = 0.7*std(x(:));
+
+%%%% add noise to X: 
+%%%% convolve with w to get Zn 
+% Xn = ...
+% w  = ....
+
+subplot(2, 2, 3)
+imagesc(Xn);
+subplot(2, 2, 4)
+imagesc(Zn, [-1 1]);
+colormap('gray')
+
+%% Exercise 4: denoising filter
+
+%%% noise level
+eta = [0 0.3 1.4]*std(x(:));
+
+%%%% loop over different noise levels
+for i = 1:3
+
+    %%%%%% TODO: compute noise filter, Wn
+    % Wn  = inv( <xn*xn> )*<xxn'>
+
+    % Wn = 
+
+    % Wcombined = 
+
+    wcombined = reshape(Wcombined(:, 78), 12, 12);
+    
+    subplot(1, 3, i)
+    imagesc(wcombined); axis square; axis off;
+end
+
+
+%% %% Exercise 5:
+eta = 0.7*std(x(:));
+% Wn =
+
+% Wcombined = WZ*Wn;
+
+w = reshape(Wcombined(78, :), 12, 12);
+
+ Z = conv2(X, w, 'same');
+
+% noisy version of stimulus
+% Xn = 
+
+% convolve w with Xn to get response
+% Zn = 
+
+figure('Name', 'images')
+subplot(2, 2, 1)
+imagesc(X);
+subplot(2, 2, 2)
+imagesc(Z);
+subplot(2, 2, 3)
+imagesc(Xn);
+subplot(2, 2, 4)
+imagesc(Zn);
+colormap('gray')
+
+%% plot the ICA filters
+figure('Name', 'ICA filters')
+for i = 1:25
+    subplot(5, 5, i)
+    imagesc(reshape(WI(i, :), 12, 12)); colormap('gray'); axis off; axis square
+end
+
+%% Exercise 6: plot a histogram of Z and ZICA
+
+grd = -100:1:100; % grid for histogram
+
+% Z = 
+% ZICA = 
+
+% pz = 
+% pr = 
+
+figure('Name', 'responses')
+semilogy(grd, pz, 'k'); hold on
+semilogy(grd, pr, 'r'); hold on
+legend('decorrelated', 'independent component')
+
+%% plot conditional histogram of  
+
+pz1z2 = hist3(ZICA([9, 10], :)', {grd' grd'});
+
+% TODO compute p(z_2)
+% pz2 = 
+
+% TODO compute p(z_1|z_2)
+% pz1_z2 = 
+
+figure('Name', 'conditional histogram')
+imagesc(grd, grd, log(pz1_z2+0.01))
+set(gca, 'Xlim', [-10 10], 'Ylim', [-10 10])
+colormap('gray')
