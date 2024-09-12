8020542f6042efc8c7570218544ceccc600946d0
Guilherme Nunes Trofino <guitrofino@gmail.com>
2024-09-12 15:05:50 +0200

    [*] MA201_project files moved to submodule

--- 2A/MA201/project/savePlot.m
+++
@@ -1,7 +0,0 @@
-function savePlot(saveName)
-
-    fileType = 'png';
-    pPath = 'C:\Users\Admin-PC\Documents\git_repository\classes_ensta\2A\MA201\project\images\';
-
-    saveas(gca, fullfile(pPath, saveName), fileType);
-end
