30bddf2181073defdca927c2496c624bf49fb006
Guilherme Nunes Trofino <guitrofino@gmail.com>
2024-12-20 12:54:39 +0100

    [+] 5RO16_TP2 main.py add compute_obrrt function

--- 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP2/src/main.py
+++ 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP2/src/main.py
@@ -15,6 +15,7 @@ import time
 import numpy as np
 import plotting, utils
 import env
+import obrrt
 import rrt
 import rrt_star
 
@@ -72,6 +73,57 @@ def add_data(
 
     except Exception as e:
         raise Exception("Error saving data: {}".format(str(e)))
+
+
+
+
+def compute_obrrt(
+        environment: env,
+        start: tuple[float] = (2, 2),
+        goal: tuple[float] = (49, 24),
+        step: float = 2,
+        goal_sample_rate: float = 0.00,
+        corner_sample_rate: float = 0.10,
+        max_iterations: int = 1500,
+        show_animation: bool = False,
+    ) -> None:
+    """
+    Execute Rapidly Exploring Random Trees alogorithm.
+
+    Args:
+        environment (env) : simulation environment.
+        start (tuple[float]) : start coordinates. Default value is '(2, 2)'.
+        goal (tuple[float]) : goal coordinates. Default value is '(49, 24)'.
+        step (int) : execution step size. Default value is '2'.
+        goal_sample_rate (float) : percetage of goal sample rate. Default value is '0.00'.
+        max_iterations (int) : iterations limit. Default value is '1500'.
+        show_animation (bool) : show execution animation? Default value is 'False'.
+    """
+    start_time = time.time()
+
+    rrt_object = obrrt.ObRrt(environment, start, goal, step, goal_sample_rate, corner_sample_rate, max_iterations)
+    path, nb_iter = rrt_object.planning()
+
+    end_time = time.time()
+
+    add_data([
+        end_time - start_time,
+        'OBRRT',
+        step,
+        goal_sample_rate,
+        max_iterations,
+        nb_iter,
+        rrt.get_path_length(path) if path else 00.0000
+    ])
+
+    if show_animation:
+        rrt_object.plotting.animation(rrt_object.vertex, path if path else [], "OBRRT", True)
+
+        plotting.plt.savefig('rrt.png')
+        plotting.plt.show()
+
+    plotting.plt.close()
+
 
 def compute_rrt(
         environment: env,
