253295b4f858c2e9843583dfa9fdbb10d5cbd0d3
Guilherme Nunes Trofino <guitrofino@gmail.com>
2024-12-24 17:27:31 +0100

    [+] 5RO16_TP3 implement BicycleToPointControl.m

--- 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPointControl.m
+++ 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPointControl.m
@@ -5,7 +5,25 @@ function [ u ] = BicycleToPointControl( xTrue,xGoal )
 %   u is the control : [v phi]'
 
 
-% TODO
+  x = xTrue(1);
+  y = xTrue(2);
+  theta = xTrue(3);
+
+  x_delta = xGoal(1) - x;
+  y_delta = xGoal(2) - y;
+
+
+  rho = sqrt( (x_delta)^2 + (y_delta)^2 );
+  alpha = AngleWrap( atan2(y_delta, x_delta) - theta );
+
+  K_rho = 20;
+  K_alpha = 10;
+
+
+  v = K_rho * rho;
+  phi = K_alpha * alpha;
+
+  u = [v, phi];
 
 end
 
