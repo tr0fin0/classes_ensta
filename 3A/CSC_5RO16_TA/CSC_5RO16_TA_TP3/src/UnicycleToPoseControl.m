f1f4c96590b28d7eab3de66a0beee5b315582da8
Guilherme Nunes Trofino <guitrofino@gmail.com>
2024-12-24 17:29:49 +0100

    [+] 5RO16_TP3 implement UnicycleToPoseControl.m

--- 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/UnicycleToPoseControl.m
+++ 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/UnicycleToPoseControl.m
@@ -1,10 +1,40 @@
-function [ u ] = UnicycleToPoseControl( xTrue,xGoal )
+function [ u ] = UnicycleToPoseControl( xTrue, xGoal )
 %Computes a control to reach a pose for unicycle
 %   xTrue is the robot current pose : [ x y theta ]'
 %   xGoal is the goal point
 %   u is the control : [v omega]'
 
-% TODO
+  x = xTrue(1);
+  y = xTrue(2);
+  theta = xTrue(3);
+
+  x_delta = xGoal(1) - x;
+  y_delta = xGoal(2) - y;
+
+  rho = sqrt( (x_delta)^2 + (y_delta)^2 );
+  alpha = AngleWrap( atan2(y_delta, x_delta) - theta );
+
+
+  K_rho = 10;
+  K_alpha = 25;
+  K_betta = 30;
+  alpha_maximum = pi / 6;
+
+
+  v = K_rho * rho;
+
+  if abs(alpha) > alpha_maximum
+    v = 0;
+  endif
+
+
+  if rho > 0.05
+    omega = K_alpha * alpha;
+  else
+    omega = K_betta * AngleWrap( xGoal(3) - theta );
+  endif
+
+  u = [v, omega];
 
 end
 
