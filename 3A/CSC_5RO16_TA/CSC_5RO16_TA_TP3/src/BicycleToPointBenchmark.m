26f2b64abb6c88d397ef679808a0fb8909cf5a63
Guilherme Nunes Trofino <guitrofino@gmail.com>
2024-12-24 17:27:10 +0100

    [+] 5RO16_TP3 add plot BicycleToPathBenchmark.m

--- 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPointBenchmark.m
+++ 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPointBenchmark.m
@@ -11,21 +11,32 @@ Perf=[];
 % loop from starting positions
 for i=1:size(Starts,2)
     xTrue=Starts(:,i);
+    XStore = NaN*zeros(3,10000);
+    XErrStore = NaN*zeros(5,10000);
     k=1;
     while max(abs(dist(xTrue(1:2),xGoal(1:2))))>.005 && k<10000
-        
+
         % Compute Control
         u=BicycleToPointControl(xTrue,xGoal);
-        
+
         % Simulate Vehicle motion
         xTrue = SimulateBicycle(xTrue,u);
-        
+
         k=k+1;
+
+        XErrStore(:,k) = [dist(xTrue,xGoal);u(1);u(2)];
+        XStore(:,k) = xTrue;
+
+        % plot every 100 updates
+        if(mod(k-2,100)==0)
+            DoBicycleGraphics(xTrue,XStore,XErrStore);
+            drawnow;
+        end;
     end;
-    
+
     % Store performances
     Perf=[Perf k];
-    
+
 end;
 % Display mean performances
 disp(['Mean goal reaching time : ', num2str(mean(Perf))]);
