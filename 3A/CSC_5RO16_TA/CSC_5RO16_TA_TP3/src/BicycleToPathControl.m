4c12ab25afd6e36bedd2056d14639fadfdde7a9c
Guilherme Nunes Trofino <guitrofino@gmail.com>
2024-12-24 17:26:24 +0100

    [+] 5RO16_TP3 implement BicycleToPathControl.m

--- 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPathControl.m
+++ 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPathControl.m
@@ -5,7 +5,44 @@ function [ u ] = BicycleToPathControl( xTrue, Path )
 %                                               y1 y2 ...]
 %   u is the control : [v phi]'
 
-% TODO
+    x = xTrue(1);
+    y = xTrue(2);
+    theta = xTrue(3);
+
+
+    distances = [];
+    for point = Path
+      x_path = point(1);
+      y_path = point(2);
+
+      distance = sqrt( ( x_path - x )^2 + ( y_path - y )^2 );
+      distances = [distances, distance];
+    endfor
+
+    [~, point_index] = min(distances);
+
+
+    point_index = min(length(distances), point_index+1);
+
+
+    x_delta = Path(1, point_index) - x;
+    y_delta = Path(2, point_index) - y;
+
+    rho = sqrt( (x_delta)^2 + (y_delta)^2 );
+    alpha = AngleWrap( atan2(y_delta, x_delta) - theta );
+
+    rho_reference = 0.5;
+    K_rho = 25;
+    K_alpha = 15;
+
+    if rho > rho_reference
+      v = K_rho * rho;
+      phi = K_alpha * alpha;
+    else
+      v = 10;
+      phi = 0;
+    endif
+
+    u = [ v phi ];
 
 end
-
