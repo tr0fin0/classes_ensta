ed18dbc7bfcf66f099d3a9e89dc176f6f24eca40
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-01-15 15:21:51 +0100

    [*] 5RO16_TP3 BicycleToPoseControl.m add funcion variables

--- 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPoseControl.m
+++ 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPoseControl.m
@@ -1,8 +1,8 @@
-function [ u ] = BicycleToPoseControl( xTrue,xGoal )
-%Computes a control to reach a pose for bicycle
-%   xTrue is the robot current pose : [ x y theta ]'
-%   xGoal is the goal point
-%   u is the control : [v phi]'
+function [ u ] = BicycleToPoseControl( xTrue, xGoal, K_rho, K_alpha, K_beta )
+  %Computes a control to reach a pose for bicycle
+  %   xTrue is the robot current pose : [ x y theta ]'
+  %   xGoal is the goal point
+  %   u is the control : [v phi]'
 
   x = xTrue(1);
   y = xTrue(2);
@@ -11,20 +11,12 @@ function [ u ] = BicycleToPoseControl( xTrue,xGoal )
   x_delta = xGoal(1) - x;
   y_delta = xGoal(2) - y;
 
-
   rho = sqrt( (x_delta)^2 + (y_delta)^2 );
   alpha = AngleWrap( atan2(y_delta, x_delta) - theta );
-  betta = AngleWrap( xGoal(3) - atan2(y_delta, x_delta) );
-
-  K_rho = +25.0;
-  K_alpha = +10.0;
-  K_betta = -05.0;
-
+  beta = AngleWrap( xGoal(3) - atan2(y_delta, x_delta) );
 
   v = K_rho * rho;
-  phi = K_alpha * alpha + K_betta * betta;
+  phi = K_alpha * alpha + K_beta * beta;
 
   u = [v, phi];
-
 end
-
