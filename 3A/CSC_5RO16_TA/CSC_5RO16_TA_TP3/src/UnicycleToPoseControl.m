18e78ca449546452e49bc54e00064b26ff905c8c
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-01-15 15:17:39 +0100

    [*] 5RO16_TP3 UnicycleToPoseControl.m add function variables

--- 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/UnicycleToPoseControl.m
+++ 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/UnicycleToPoseControl.m
@@ -1,8 +1,8 @@
-function [ u ] = UnicycleToPoseControl( xTrue, xGoal )
-%Computes a control to reach a pose for unicycle
-%   xTrue is the robot current pose : [ x y theta ]'
-%   xGoal is the goal point
-%   u is the control : [v omega]'
+function [ u ] = UnicycleToPoseControl( xTrue, xGoal, alpha_maximum, K_rho, K_alpha, K_beta )
+  %Computes a control to reach a pose for unicycle
+  %   xTrue is the robot current pose : [ x y theta ]'
+  %   xGoal is the goal point
+  %   u is the control : [v omega]'
 
   x = xTrue(1);
   y = xTrue(2);
@@ -14,27 +14,17 @@ function [ u ] = UnicycleToPoseControl( xTrue, xGoal )
   rho = sqrt( (x_delta)^2 + (y_delta)^2 );
   alpha = AngleWrap( atan2(y_delta, x_delta) - theta );
 
-
-  K_rho = 10;
-  K_alpha = 25;
-  K_betta = 30;
-  alpha_maximum = pi / 6;
-
-
   v = K_rho * rho;
 
   if abs(alpha) > alpha_maximum
     v = 0;
   endif
 
-
   if rho > 0.05
     omega = K_alpha * alpha;
   else
-    omega = K_betta * AngleWrap( xGoal(3) - theta );
+    omega = K_beta * AngleWrap( xGoal(3) - theta );
   endif
 
   u = [v, omega];
-
 end
-
