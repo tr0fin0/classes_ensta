044a62065c5d973cc6eb1e369419053ae417cc4b
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-01-15 15:20:49 +0100

    [*] 5RO16_TP3 BicycleToPointControl.m add funcion variables

--- 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPointControl.m
+++ 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP3/src/BicycleToPointControl.m
@@ -1,9 +1,8 @@
-function [ u ] = BicycleToPointControl( xTrue,xGoal )
-%Computes a control to reach a pose for bicycle
-%   xTrue is the robot current pose : [ x y theta ]'
-%   xGoal is the goal point
-%   u is the control : [v phi]'
-
+function [ u ] = BicycleToPointControl( xTrue, xGoal, K_rho, K_alpha )
+  %Computes a control to reach a pose for bicycle
+  %   xTrue is the robot current pose : [ x y theta ]'
+  %   xGoal is the goal point
+  %   u is the control : [v phi]'
 
   x = xTrue(1);
   y = xTrue(2);
@@ -12,18 +11,11 @@ function [ u ] = BicycleToPointControl( xTrue,xGoal )
   x_delta = xGoal(1) - x;
   y_delta = xGoal(2) - y;
 
-
   rho = sqrt( (x_delta)^2 + (y_delta)^2 );
   alpha = AngleWrap( atan2(y_delta, x_delta) - theta );
-
-  K_rho = 20;
-  K_alpha = 10;
-
 
   v = K_rho * rho;
   phi = K_alpha * alpha;
 
   u = [v, phi];
-
 end
-
