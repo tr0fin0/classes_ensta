dc08a5706da86f797cf86dc5a4a47149eeff7e9d
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-01-20 09:17:25 +0100

    [*] 5RO16_TP4 plot_zone.m format file

--- 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP4/src/plot_zone.m
+++ 3A/CSC_5RO16_TA/CSC_5RO16_TA_TP4/src/plot_zone.m
@@ -1,12 +1,12 @@
 function plot_zone()
-  figure()
-  for i=-2:0.1:2
-    for j=-2:0.1:2
-      if verif_stability([i;j])
-        plot(i,j,'+');
-        hold on
-      endif
+    figure()
+
+    for i = -2:0.1:+2
+        for j = -2:0.1:+2
+            if verify_stability([i; j])
+                plot(i, j, '+');
+                hold on
+            endif
+        endfor
     endfor
-  endfor
-  
 endfunction
