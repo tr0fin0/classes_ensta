6e92bf14b1d4ac1ea103075d7d3a6be78cd4ff32
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-01-31 10:00:10 +0100

    [*] 52081_TP1 environment variables refactoring

--- 3A/CSC_52081_EP/CSC_52081_EP_TP1/environment.py
+++ 3A/CSC_52081_EP/CSC_52081_EP_TP1/environment.py
@@ -1,151 +1,195 @@
+from matplotlib.colors import ListedColormap
+
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 import matplotlib.patches as mpatches
 
+
+
 class Environment():
-
-    def __init__(self, G, theta = [0.9,0.8]):
+    def __init__(
+            self, grid: np.ndarray[float], noises: list[float] = [0.9, 0.8], fps: bool = False
+        ):
         '''
-            Environment.
+        Environment initialization.
 
-            Parameters
-            ----------
-
-            G : array_like(int, ndim=2) of shape (n_rows,n_columns)
-                Specifies a grid where G[j,k] = entry & sound1 & sound2
-
-            theta : array_like(float, ndim=1)
-                Specifies the grid dynamics (acoustics)
-
+        Args:
+            grid (np.ndarray[float]): n x m matrix defining environment
+            noises (list[float]): noise occurrence probability vector. Default is '[0.9, 0.8]'
+            fps (bool): environment allows false positives? Default is 'False'.
         '''
-        # Grid
-        self.G = G
+        # Grid Initialization
+        self.grid = grid
 
-        # Grid shape
-        self.n_rows = G.shape[0]
-        self.n_cols = G.shape[1]
-        self.n_states = self.n_cols * self.n_rows
+        self.n_rows = self.grid.shape[0]
+        self.n_columns = self.grid.shape[1]
+        self.n_states = int(self.n_rows * self.n_columns)
 
-        # State space - tile number representation
+        # State Space Initialization
         self.labels = np.arange(1,self.n_states+1)
-        self.obs = np.arange(2)
 
-        # Setting Observation Matrix
-        self.theta = theta
+        self.noises = noises
+        self.n_noises = len(self.noises)
 
-        # Set State Transition Matrix (and Prior Vector and Observation Matrix)
-        self.P_S = np.zeros((self.n_states,self.n_states))
+        self.false_positives = fps
+
+        # State Transition Matrix, probability from moving between states
+        self.P_S = np.zeros((self.n_states, self.n_states))
+
+        # Prior Vector, initial state probability
         self.P_1 = np.zeros(self.n_states)
-        self.d_obs = len(theta)
-        theta = np.array(theta)
-        Theta = np.vstack((1 - theta, theta)).T
-        self.P_O = np.zeros((self.n_states,self.d_obs,2))
-        for s in range(self.n_states):
-            i,j = divmod(s, self.n_cols) 
-            if self.G[i,j] > 3:
-                self.P_1[s] = 1
-            # By default, P(0 = 0) = 1
-            self.P_O[s,0] = [1,0]
-            self.P_O[s,1] = [1,0]
-            if self.G[i,j] == 2 or self.G[i,j] == 3:
-                self.P_O[s,0] = Theta[0]
-            if self.G[i,j] == 1 or self.G[i,j] == 3:
-                self.P_O[s,1] = Theta[1]
-            if i > 0:
-                self.P_S[s,s-self.n_cols] = 1
-            if i < self.n_rows - 1:
-                self.P_S[s,s+self.n_cols] = 1
-            if j > 0:
-                self.P_S[s,s-1] = 1
-            if j < self.n_cols - 1 and s < self.n_states - 1:
-                self.P_S[s,s+1] = 1
-            self.P_S[s,:] = self.P_S[s,:] / sum(self.P_S[s,:])
 
-            assert(sum(self.P_S[s]) == 1)
-            assert(sum(self.P_O[s,0]) == 1)
-            assert(sum(self.P_O[s,1]) == 1)
+        # Observation Matrix, conditional probability of observing a feature
+        self.P_O = np.zeros((self.n_states, self.n_noises, 2))
+
+
+        arr_noises = np.vstack((1 - np.array(self.noises), np.array(self.noises))).T
+
+        for state in range(self.n_states):
+            i, j = divmod(state, self.n_columns)
+
+            # entry point
+            if self.grid[i, j] > 3:
+                self.P_1[state] = 1
+
+            if self.false_positives:
+                # 10% chance of false positive
+                self.P_O[state, 0] = [0.9, 0.1]
+                self.P_O[state, 1] = [0.9, 0.1]
+            else:
+                self.P_O[state, 0] = [1, 0]
+                self.P_O[state, 1] = [1, 0]
+
+            # sound rustle
+            if self.grid[i, j] == 2 or self.grid[i, j] == 3:
+                self.P_O[state, 0] = arr_noises[0]
+
+            # sound crinkle
+            if self.grid[i, j] == 1 or self.grid[i, j] == 3:
+                self.P_O[state, 1] = arr_noises[1]
+
+            # state transition
+            if i > 0:
+                self.P_S[state, state - self.n_columns] = 1
+
+            if i < self.n_rows - 1:
+                self.P_S[state, state + self.n_columns] = 1
+
+            if j > 0:
+                self.P_S[state, state - 1] = 1
+
+            if j < self.n_columns - 1 and state < self.n_states - 1:
+                self.P_S[state, state + 1] = 1
+            self.P_S[state, :] = self.P_S[state, :] / sum(self.P_S[state, :])
+
+            # probability normalization
+            assert(sum(self.P_S[state]) == 1)
+            assert(sum(self.P_O[state, 0]) == 1)
+            assert(sum(self.P_O[state, 1]) == 1)
+
         self.P_1 = self.P_1 / sum(self.P_1)
+
+
+    def __str__(self) -> str:
+        '''
+        Return a string representation of class environment.
+        '''
+        description = f"{self.__class__.__name__}:\n"
+        description += f"\tFPS:\t[{int(self.false_positives):4d}]\n"
+        description += f"\tnoises:\t[{self.n_noises:4d}] {self.noises}\n"
+        description += f"\tlabels:\t[{self.n_states:4d}] \n"
+        description += f"\tP_S:\n"
+
+
+        rows = []
+        for row in self.P_S:
+            rows.append(" ".join(f"{value:.2f}" for value in row))
+
+        description += "\n".join(rows)
+
+        return description
+
 
     def rwd(self, s, a):
         '''
-            Reward function r(s, a) of taking action a when in state s
+        Reward function r(s, a) of taking action a when in state s
 
-            Parameters
-            ----------
-            s : int
-                true state (tile which containts the object)
-            a : int
-                estimated state
+        Parameters
+        ----------
+        s : int
+            true state (tile which contains the object)
+        a : int
+            estimated state
 
-            Returns
-            -------
-            float
-                reward obtained from taking action a given state s
+        Returns
+        -------
+        float
+            reward obtained from taking action a given state s
+    '''
+        return (s==a)*1.
+
+    def step(self, previous_state: int | None = None) -> tuple[int, np.ndarray[int]]:
         '''
-        return (s==a)
+        Return next state s and observation o from previous state previous_state.
 
-    def step(self, _s=None):
-        ''' Step to the state, given prev state _s.
+        Args:
+            previous_state (int | None): previous state. Default is 'None'.
 
-            Paramaters
-            ----------
-
-            _s : int
-                prev state
-
-            Returns
-            -------
-
-            s : int
-                next state
-            o : array-like (2)
-                corresponding observation
+        Returns:
+            s (int) : next state.
+            o (np.ndarray[int]) : state observation
         '''
-
-        # Generate a state s' ~ p( . | s)
-
-        if _s is None:
-            s = np.random.choice(self.n_states,p=self.P_1)
+        # Generate a State: s' ~ p( . | s)
+        if previous_state is None:
+            next_state = np.random.choice(self.n_states, p=self.P_1)
         else:
-            s = np.random.choice(self.n_states,p=self.P_S[_s,:])
+            next_state = np.random.choice(self.n_states, p=self.P_S[previous_state,:])
 
-        # Generate an observation o' ~ p(. | s')
-        
-        o = np.zeros(self.d_obs)
-        for j in range(self.d_obs):
-            w = self.P_O[s,j]
+        # Generate an Observation: o' ~ p(. | s')
+        observation = np.zeros(self.n_noises)
+        for j in range(self.n_noises):
+            w = self.P_O[next_state, j]
+            observation[j] = np.random.choice(self.n_noises, p = w)
+
             #print("P(O | s=%s) = %s" % (str(s),str(self.P_O[s,j,:])))
-            o[j] = np.random.choice(self.d_obs,p=w)
 
-        return s, o
+        return next_state, observation
 
 
     def tile2cell(self, s):
-        return divmod(s, self.n_cols) 
+        return divmod(s, self.n_columns)
 
-    def render(self, y_seq=None, x_seq=None, dgrid=None, a_star=None, paths=[], title=None, add_legend=True, output_fname=None):
+    def render(
+        self,
+        y_seq=None,
+        x_seq=None,
+        dgrid: np.ndarray[float] = None,
+        a_star=None,
+        paths=[],
+        title=None,
+        add_legend=True,
+        output_fname=None
+    ):
         '''
-            Plot a visual representation of the environment.
+        Plot a visual representation of the environment.
 
-            Parameters
-            ----------
+        Parameters
+        ----------
 
-            y_seq : numpy array (dtype=int)
-                a path (e.g., [1,3,1,2])
+        y_seq : numpy array (dtype=int)
+            a path (e.grid., [1,3,1,2])
 
-            x_seq :
-                observations associated with the path
+        x_seq :
+            observations associated with the path
 
-            dgrid : shape like self.G
-                contains values (e.g., probabilities) to show in each tile
+        dgrid : shape like self.grid
+            contains values (e.grid., probabilities) to show in each tile
 
-            a_star : int
-                the optimal action
+        a_star : int
+            the optimal action
 
-            title : str
-                a title for the plot
-
+        title : str
+            a title for the plot
         '''
 
         fig, ax = plt.subplots(figsize=[8,4])
@@ -163,13 +207,13 @@ class Environment():
 
         if dgrid is None:
             # ... as a visual representation
-            im = ax.imshow(self.G, cmap=ListedColormap(list(colors.values())), alpha=0.3)
+            im = ax.imshow(self.grid, cmap=ListedColormap(list(colors.values())), alpha=0.3)
             patches = [mpatches.Patch(color=colors[i], alpha=0.3, label=labels[i]) for i in [1,2,3,4]]
             if add_legend:
                 plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
         else:
             # ... as a probability mass function
-            im = ax.imshow(dgrid.reshape(self.n_rows,self.n_cols), cmap=plt.cm.Reds)
+            im = ax.imshow(dgrid.reshape(self.n_rows, self.n_columns), cmap=plt.cm.Reds)
 
         # Plot the path, alongside the observations generated by that path, and the optimal action.
 
@@ -205,9 +249,9 @@ class Environment():
 
         # Ticks and grid
 
-        ax.set_xticks(np.arange(0, self.n_cols, 1))
-        ax.set_xticks(np.arange(-0.5, self.n_cols, 1), minor=True)
-        ax.set_xticklabels(np.arange(0, self.n_cols, 1))
+        ax.set_xticks(np.arange(0, self.n_columns, 1))
+        ax.set_xticks(np.arange(-0.5, self.n_columns, 1), minor=True)
+        ax.set_xticklabels(np.arange(0, self.n_columns, 1))
 
         ax.set_yticks(np.arange(0, self.n_rows, 1))
         ax.set_yticks(np.arange(-0.5, self.n_rows, 1), minor=True)
@@ -217,7 +261,7 @@ class Environment():
 
         n = 0
         for i in range(self.n_rows):
-            for j in range(self.n_cols):
+            for j in range(self.n_columns):
               ax.text(j, i, n, va='center', ha='center')
               n = n + 1
 
