5e1b13f250e9297a095b4738a8c8e379e7f737d8
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-03-06 09:31:02 +0100

    [*] 5RO05_TP2 td2c update formatting

--- 3A/CSC_5RO05_TA/CSC_5RO05_TA_TD02/src/td2c.cpp
+++ 3A/CSC_5RO05_TA/CSC_5RO05_TA_TD02/src/td2c.cpp
@@ -1,31 +1,37 @@
 #include "Looper.h"
 #include <iostream>
-#include <cstdlib>   // For atoi()
-#include <ctime>     // For clock_gettime()
+#include <cstdlib>
+#include <ctime>
 
-// Function to get time in seconds
-double getTime() {
+
+double getTime()
+{
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
+
     return ts.tv_sec + ts.tv_nsec * 1e-9;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <nLoops>" << std::endl;
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <nLoops>\n", argv[0]);
+
         return 1;
     }
 
-    double nLoops = std::atoi(argv[1]);  // Convert input to integer
+    double nLoops = std::atoi(argv[1]);
 
     Looper looper;
-    
-    double startTime = getTime();  // Start timing
-    looper.runLoop(nLoops);
-    double endTime = getTime();    // End timing
 
-    std::cout << "nLoops: " << nLoops << std::endl;
-    std::cout << "Execution time: " << (endTime - startTime) << " seconds" << std::endl;
+    double startTime = getTime();
+    looper.runLoop(nLoops);
+    double endTime = getTime();
+
+    printf("nLoops: %f\n", nLoops);
+    printf("Execution time: %f seconds\n", endTime - startTime);
 
     return 0;
 }
