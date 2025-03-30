bf27c875300637f95fb4409f4dd744131f3fb442
Guilherme Nunes Trofino <guitrofino@gmail.com>
2025-03-30 09:10:13 +0200

    [+] 5RO05_TP4 add timespec.h

---
+++ 3A/CSC_5RO05_TA/CSC_5RO05_TA_TD04/src/timespec.h
@@ -0,0 +1,25 @@
+#include <time.h>
+#include <thread>
+
+// conversion
+double timespec_to_ms(const timespec& time_ts);
+timespec timespec_from_ms(double time_ms);
+timespec timespec_normalize(const timespec& time_ts);
+
+timespec timespec_now();
+timespec timespec_negate(const timespec& time_ts);
+
+timespec timespec_add(const timespec& time1_ts, const timespec& time2_ts);
+timespec timespec_subtract(const timespec& time1_ts, const timespec& time2_ts);
+
+void timespec_wait(const timespec& delay_ts);
+
+timespec  operator- (const timespec& time_ts);
+timespec  operator+ (const timespec& time1_ts, const timespec& time2_ts);
+timespec  operator- (const timespec& time1_ts, const timespec& time2_ts);
+timespec& operator+= (timespec& time_ts, const timespec& delay_ts);
+timespec& operator-= (timespec& time_ts, const timespec& delay_ts);
+bool operator== (const timespec& time1_ts, const timespec& time2_ts);
+bool operator!= (const timespec& time1_ts, const timespec& time2_ts);
+bool operator< (const timespec& time1_ts, const timespec& time2_ts);
+bool operator> (const timespec& time1_ts, const timespec& time2_ts);
